bc8e74e82de4f47771e1a330ac21eccbcd12ed53
Brian Takita <brian.takita@gmail.com>
2024-03-12 03:54:22 -0400

    ./app:
    	+ _style:
    		+ rangeslider.css
    		+ theme.css
    		+ toggle-switch.css
    		+ utility-patterns.css
    	.:
    		browser:
    			+ ∋ @btakita/ui--browser--peakexistence/home/hyop
    			+ ∋ @btakita/ui--browser--peakexistence/layout/hyop
    			+ ∋ aos: + Aos.init
    			+ ∋ hyop
    		css: ∋ _style/*.css
    		server: /:
    			logo_image: ∋ fa_waves_sine_
    			- ∋ csrf_403_response_
    			∋ home__doc_html_
    
    ./config:
    
    	∋ relement__use: ∋ server__relement
    	∋ fa_waves_sine_
    
    ./tailwind.config: + cruip simple theme
    
    dependencies:
    
    	+ ∋ aos
    	+ ∋ @btakita/ui--server--peakexistence
    	+ ∋ @tailwind/forms
    	+ ∋ @types/aos
    	update dependencies

--- app/index.server.ts
+++ app/index.server.ts
@@ -1,23 +1,20 @@
 import './index.css'
-import { csrf_403_response_ } from '@rappstack/domain--server--auth/auth'
+import { home__doc_html_ } from '@btakita/ui--server--peakexistence/home'
+import { fa_waves_sine_ } from '@btakita/ui--server--peakexistence/icon'
 import { site_request_ctx__ensure } from '@rappstack/domain--server/ctx'
 import { Elysia } from 'elysia'
 import { middleware_ } from 'rebuildjs/server'
 import { html_response__new } from 'relysjs/server'
-import { logo_image, site, social_a1 } from '../config.js'
+import { site, social_a1 } from '../config.js'
 export default middleware_(middleware_ctx=>
 	new Elysia({
 		name: 'root_routes'
 	})
 		.get('/', async context=>{
 			const ctx = site_request_ctx__ensure(middleware_ctx, context, {
-				logo_image,
+				logo_image_: fa_waves_sine_,
 				site,
 				social_a1
 			})
-			if (csrf_403_response_(ctx)) {
-				return csrf_403_response_(ctx)
-			}
-			return html_response__new(
-				'<h1 class="font-bold">Hello<h1/>')
+			return html_response__new(home__doc_html_({ ctx }))
 		}))
--- config.ts
+++ config.ts
@@ -1,26 +1,19 @@
+import { fa_waves_sine_ } from '@btakita/ui--server--peakexistence/icon'
 import { type site_T } from '@rappstack/domain--server/site'
 import { type social_T } from '@rappstack/domain--server/social'
 import { import_meta_env_ } from 'ctx-core/env'
 import { relement__use } from 'relementjs'
 import { server__relement } from 'relementjs/server'
 import { app_ctx, cwd__set, port__set, src_path__set } from 'relysjs/server'
-import waves_sine_svg from '../../public/image/icon/waves-sine.svg'
-export const logo_image = {
-	enable: true,
-	svg: true,
-	src: waves_sine_svg,
-	width: 48,
-	height: 48,
-}
+relement__use(server__relement)
 export const site:site_T = {
 	website: 'https://peakexistence.com',
 	author: 'Dr. Manny Emenike',
 	description: 'Experience Your Peak Existence',
 	title: 'Peak Existence',
-	og_image: waves_sine_svg,
+	og_image: '' + fa_waves_sine_(),
 }
-export const social_a1:social_T[] = [
-]
+export const social_a1:social_T[] = []
 export function config__init() {
 	const port = parseInt(import_meta_env_().PEAKEXISTENCE_PORT) || 4102
 	cwd__set(app_ctx, process.cwd())
