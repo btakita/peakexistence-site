5c10376c77d0eb363cc856d15cf55426ad3b9f3c
Brian Takita <brian.takita@gmail.com>
2024-03-12 16:39:31 -0400

    ./config: site:
    	- og_image
    	+ font__meta_props_a1
    	+ icon
    	+ social_image_url
    	+ light_and_dark_mode
    	+ google_site_verification
    	+ gtag_id
    	+ body_class

--- config.ts
+++ config.ts
@@ -1,7 +1,8 @@
-import { fa_waves_sine_ } from '@btakita/ui--server--peakexistence/icon'
+import fa_waves_sine_svg from '@btakita/ui--server--peakexistence/icon/fa_waves_sine.svg.file'
 import { type site_T } from '@rappstack/domain--server/site'
 import { type social_T } from '@rappstack/domain--server/social'
 import { import_meta_env_ } from 'ctx-core/env'
+import { class_ } from 'ctx-core/html'
 import { relement__use } from 'relementjs'
 import { server__relement } from 'relementjs/server'
 import { app_ctx, cwd__set, port__set, src_path__set } from 'relysjs/server'
@@ -11,7 +12,28 @@ export const site:site_T = {
 	author: 'Dr. Manny Emenike',
 	description: 'Experience Your Peak Existence',
 	title: 'Peak Existence',
-	og_image: '' + fa_waves_sine_(),
+	font__meta_props_a1: [
+		{ rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+		{ rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 1 },
+		{
+			href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;800;900&display=fallback',
+			rel: 'stylesheet'
+		}
+	],
+	icon: {
+		type: 'image/svg+xml',
+		href: fa_waves_sine_svg
+	},
+	social_image_url: fa_waves_sine_svg,
+	light_and_dark_mode: true,
+	google_site_verification: import_meta_env_().PUBLIC_GOOGLE_SITE_VERIFICATION,
+	gtag_id: 'G-F2F171MSE3',
+	body_class:class_(
+		'font-inter',
+		'antialiased',
+		'bg-white',
+		'text-gray-900',
+		'tracking-tight')
 }
 export const social_a1:social_T[] = []
 export function config__init() {
