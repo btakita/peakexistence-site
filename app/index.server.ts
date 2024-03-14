3e5e76298fa6bac2ab869b925227048736e6b402
Brian Takita <brian.takita@gmail.com>
2024-03-14 01:43:53 -0400

    ./app: server: ∋ site_request_ctx__ensure:
    	- logo_image_
    	- social_a1

--- app/index.server.ts
+++ app/index.server.ts
@@ -1,20 +1,15 @@
 import './index.css'
 import { home__doc_html_ } from '@btakita/ui--server--peakexistence/home'
-import { fa_waves_sine_ } from '@btakita/ui--server--peakexistence/icon'
 import { site_request_ctx__ensure } from '@rappstack/domain--server/ctx'
 import { Elysia } from 'elysia'
 import { middleware_ } from 'rebuildjs/server'
 import { html_response__new } from 'relysjs/server'
-import { site, social_a1 } from '../config.js'
+import { site } from '../config.js'
 export default middleware_(middleware_ctx=>
 	new Elysia({
 		name: 'root_routes'
 	})
 		.get('/', async context=>{
-			const ctx = site_request_ctx__ensure(middleware_ctx, context, {
-				logo_image_: fa_waves_sine_,
-				site,
-				social_a1
-			})
+			const ctx = site_request_ctx__ensure(middleware_ctx, context, { site })
 			return html_response__new(home__doc_html_({ ctx }))
 		}))
