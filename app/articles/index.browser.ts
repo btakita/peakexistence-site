a61f2d89c62986acf0b508b949a0f70df795db64
Brian Takita <brian.takita@gmail.com>
2024-04-11 18:37:10 -0400

    ./app: articles: browser: + ∋ blog_footnote__hyops

--- app/articles/index.browser.ts
+++ app/articles/index.browser.ts
@@ -1,3 +1,4 @@
+import * as blog_footnote__hyops from '@rappstack/ui--browser--blog/footnote/hyop'
 import * as blog_header__hyops from '@rappstack/ui--browser--blog/header/hyop'
 import * as blog_post__hyops from '@rappstack/ui--browser--blog/post/hyop'
 import * as blog_theme__hyops from '@rappstack/ui--browser--blog/theme/hyop'
@@ -5,6 +6,7 @@ import * as layout__hyops from '@btakita/ui--browser--peakexistence/layout/hyop'
 import { hyop } from 'relementjs/browser'
 window.addEventListener('load', ()=>{
 	hyop(document, {
+		...blog_footnote__hyops,
 		...blog_header__hyops,
 		...blog_post__hyops,
 		...blog_theme__hyops,
