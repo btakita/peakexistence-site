6a28c0be8568e14361b09dbccee34dc25f23f922
Brian Takita <brian.takita@gmail.com>
2024-04-12 20:08:50 -0400

    + /articles/discseel-procedure

--- article/index.ts
+++ article/index.ts
@@ -8,6 +8,7 @@ export const post_mod_a1:post_mod_T[] = await Promise.all([
 	import('./content/cardiovascular-health.js'),
 	import('./content/comprehensive-wellness-testing-and-biometric-screening.js'),
 	import('./content/crisis-stabilization-unit.js'),
+	import('./content/discseel-procedure.js'),
 	import('./content/early-disease-detection.js'),
 	import('./content/epidural-neoregeneration.js'),
 	import('./content/erectile-dysfunction-treatment.js'),
