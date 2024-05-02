f4b5d42e94ee0d318424aa4f3f0438d2b90a783c
Brian Takita <brian.takita@gmail.com>
2024-05-01 21:54:32 -0400

    ./sticky: mobile: bump up by 2px:
    	sticky__h3_class
    	sticky__h4_class
    	sticky__h5_class

--- sticky/index.ts
+++ sticky/index.ts
@@ -1,9 +1,9 @@
 import { class_ } from 'ctx-core/html'
 export const sticky__breadcrumbs__nav_class = class_('sticky', 'top-[64px]', 'sm:top-[74px]', 'bg-white', 'z-50', 'whitespace-nowrap', 'overflow-x-auto')
 export const sticky__h2_class = class_('sticky', 'top-[88px]', 'sm:top-[98px]', 'bg-white', 'z-40', 'whitespace-nowrap', 'overflow-x-auto')
-export const sticky__h3_class = class_('sticky', 'top-[120px]', 'sm:top-[130px]', 'bg-white', 'z-30', 'whitespace-nowrap', 'overflow-x-auto')
-export const sticky__h4_class = class_('sticky', 'top-[148px]', 'sm:top-[158px]', 'bg-white', 'z-20', 'whitespace-nowrap', 'overflow-x-auto')
-export const sticky__h5_class = class_('sticky', 'top-[176px]', 'sm:top-[186px]', 'bg-white', 'z-10', 'whitespace-nowrap', 'overflow-x-auto')
+export const sticky__h3_class = class_('sticky', 'top-[118px]', 'sm:top-[130px]', 'bg-white', 'z-30', 'whitespace-nowrap', 'overflow-x-auto')
+export const sticky__h4_class = class_('sticky', 'top-[146px]', 'sm:top-[158px]', 'bg-white', 'z-20', 'whitespace-nowrap', 'overflow-x-auto')
+export const sticky__h5_class = class_('sticky', 'top-[174px]', 'sm:top-[186px]', 'bg-white', 'z-10', 'whitespace-nowrap', 'overflow-x-auto')
 export const sticky__level_props_a1 = [
 	sticky__h2_class,
 	sticky__h3_class,
