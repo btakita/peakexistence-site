71c37a378ef8d3557fefcd0ad6c4fbc8981f8180
Brian Takita <brian.takita@gmail.com>
2024-05-03 01:59:19 -0400

    /articles/exercise-with-oxygen-therapy: + content,references

--- article/content/exercise-with-oxygen-therapy.ts
+++ article/content/exercise-with-oxygen-therapy.ts
@@ -1,14 +1,25 @@
 import { exercise_with_oxygen_therapy } from '@btakita/domain--server--peakexistence/feature'
 import { post_meta__validate } from '@rappstack/domain--server--blog/post'
+import { tb_a_ } from '@rappstack/ui--any/anchor'
 import { dl_tree_ } from '@rappstack/ui--any/dl'
 import { nl } from '@rappstack/ui--any/string'
+import { footnote__sup_, footnote__sup__sort } from '@rappstack/ui--server--blog/footnote'
 import { type request_ctx_T } from 'rebuildjs/server'
 import { mobile_hidden__span_ } from '../../responsive/index.js'
 import { sticky__dl_tree_props_ } from '../../sticky/index.js'
 // import exercise_with_oxygen_therapy_webp from '../../public/image/article/exercise-with-oxygen-therapy.webp'
 export const meta_ = (ctx:request_ctx_T)=>post_meta__validate(ctx, {
 	pub_date: '2024-03-22T20:42:15.614Z',
-	...exercise_with_oxygen_therapy
+	...exercise_with_oxygen_therapy,
+	description_md: [
+		`Exercise with Oxygen Therapy (EWOT) is a therapeutic technique. That combines physical activity with the inhalation with the inhalation of enriched oxygen. To enhance physiological & cognitive functions. By enhancing oxygen availability to the tissues. This method leverages the increased availability of oxygen to:`,
+		``,
+		`- improved athletic performance`,
+		`- accelerate recovery`,
+		`- support overall health`,
+		``,
+		`This article explores the concept of EWOT, its mechanisms, benefits, & potential applications in both clinical & fitness settings.`
+	].join(nl),
 })
 // @formatter:off
 // language=md
@@ -17,26 +28,52 @@ export default (ctx:request_ctx_T)=>''
 	[`## What is ${mobile_hidden__span_([`Exercise with Oxygen Therapy`, `It`])}?`, [
 		`Exercise with Oxygen Therapy (EWOT) combines physical activity with increased oxygen intake. During an EWOT session, you’ll perform simple exercises. While breathing in a higher concentration of oxygen. This process helps your body deliver more oxygen to your cells, muscles, & organs. Resulting in a range of incredible health benefits.`].join(nl)],
 	[`## The Science Behind EWOT: How it Works`, [
+		`EWOT involves breathing higher concentrations of oxygen during exercise. Ranging from 30% to 60% oxygen compared to the 21% found in ambient air. By increasing the oxygen content in the blood. More oxygen is available to muscle tissues & the brain. Enhancing the efficiency of metabolic processes & reducing the reliance on anaerobic energy pathways. Which produce lactic acid & lead to quicker fatigue${footnote__sup__sort([ref_e1_3_footnote__sup_(ctx),ref_e1_8_footnote__sup_(ctx)])}.`,
+		``,
 		`During conventional exercise, our muscles require oxygen to produce energy for movement. As we age or encounter health challenges, our body’s ability to deliver oxygen efficiently may diminish. However, EWOT addresses this by enhancing oxygen delivery, allowing your body to work at its full potential during exercise.`,
 		``,
 		`When you exercise with additional oxygen, your blood becomes oxygen-rich. Enhancing the oxygen saturation of tissues & organs. This increased oxygenation:`,
+		``,
 		`- supports cellular function`,
 		`- aids in detoxification`,
 		`- helps improve overall cardiovascular health`].join(nl)],
+	[`## Benefits of EWOT`, [
+		dl_tree_({ ctx, _: sticky__dl_tree_props_ }, ()=>[
+			[`### Improved Exercise Performance`, [
+				`Studies have shown that EWOT enhances physical performance by increasing endurance & reducing fatigue. This is particularly beneficial for athletes and individuals engaged in high-intensity workouts${footnote__sup__sort([ref_e1_3_footnote__sup_(ctx),ref_e1_11_footnote__sup_(ctx)])}.`].join(nl)],
+			[`### Accelerated Recovery`, [
+				`Oxygen plays a crucial role in recovery processes. Including the repair of muscle tissues & the reduction of inflammation. EWOT can help speed up these recovery mechanisms. Allowing for quicker restoration of muscle function & reduced soreness after exercise${footnote__sup__sort([ref_e1_3_footnote__sup_(ctx),ref_e1_11_footnote__sup_(ctx)])}. Giving you more frequent & effective exercise.`].join(nl)],
+			[`### Enhanced Cognitive Function`, [
+				`EWOT benefits physical performance & has positive effects on cognitive functions. Such as:`,
+				``,
+        `- memory`,
+        `- focus`,
+        `- reaction times`,
+				``,
+        `This is due to improved oxygenation of the brain during exercise${footnote__sup__sort([ref_e1_7_footnote__sup_(ctx),ref_e1_17_footnote__sup_(ctx)])}.`].join(nl)],
+			[`### Cardiovascular Health`, [
+				`Increased oxygen levels supports heart health. Improving blood flow & circulation. By improving oxygen delivery & reducing the workload on the heart during physical activity. EWOT contributes to better cardiovascular health. And helps in managing conditions, like hypertension & chronic heart failure${footnote__sup__sort([ref_e1_9_footnote__sup_(ctx),ref_e1_19_footnote__sup_(ctx)])}.`].join(nl)],
+			[`### Enhanced Weight Loss`, [
+				`Combining oxygen therapy with exercise can boost metabolism, aiding in weight management efforts.`].join(nl)],
+			[`### Detoxification`, [
+				`Oxygen is essential for cellular detoxification. Helping to remove waste & improve cellular health.`].join(nl)],
+			[`### Boosted Mental Clarity`, [
+				`Improved oxygenation improves cognitive function. Promoting better focus & mental clarity.`].join(nl)],
+			[`### Reduced Inflammation`, [
+				`Oxygen plays a vital role in reducing inflammation. Alleviating chronic inflammatory conditions.`].join(nl)],
+		])].join(nl)],
+	[`## Applications of EWOT`, [
+		`EWOT is used in various settings, from sports training facilities to rehabilitation centers. It is particularly effective for:`,
+		`- Athletes looking to enhance performance and reduce recovery times.`,
+		`- Individuals with chronic conditions such as COPD. Where improved oxygenation can significantly enhance life quality${footnote__sup__sort([ref_e1_1_footnote__sup_(ctx),ref_e1_2_footnote__sup_(ctx)])}.`,
+		`- Older adults and those recovering from injuries. Where it can aid in faster recovery and improve functional capacity${footnote__sup__sort([ref_e1_10_footnote__sup_(ctx),ref_e1_15_footnote__sup_(ctx)])}.`,
+	].join(nl)],
 	[`## ${mobile_hidden__span_([`Improved `])}Stamina & Endurance`, [
 		`By increasing oxygen intake during exercise, EWOT helps you push your boundaries. Leading to improved stamina & endurance over time.`].join(nl)],
-	[`## Accelerated Recovery`, [
-		`EWOT reduces recovery time after workouts. Giving you more frequent & effective exercise.`].join(nl)],
-	[`## Enhanced Weight Loss`, [
-		`Combining oxygen therapy with exercise can boost metabolism, aiding in weight management efforts.`].join(nl)],
-	[`## Cardiovascular Health`, [
-		`Increased oxygen levels support heart health, improving blood flow & circulation.`].join(nl)],
-	[`## Detoxification`, [
-		`Oxygen is essential for cellular detoxification. Helping to remove waste & improve cellular health.`].join(nl)],
-	[`## Boosted Mental Clarity`, [
-		`Improved oxygenation improves cognitive function. Promoting better focus & mental clarity.`].join(nl)],
-	[`## Reduced Inflammation`, [
-		`Oxygen plays a vital role in reducing inflammation. Alleviating chronic inflammatory conditions.`].join(nl)],
+	[`## Implementing EWOT`, [
+		`Individuals typically use an oxygen concentrator. Connected to a mask or nasal cannula during exercise sessions. It is important to start with moderate intensities to assess tolerance. Increasing the duration & intensity of workouts under the guidance of a healthcare professional.`].join(nl)],
+	[`## Conclusion`, [
+		`Exercise with Oxygen Therapy offers a multifaceted approach to enhancing physical & mental health. Through the synergistic effects of increased oxygen intake and physical activity. As research continues, the potential applications of EWOT expand. Promising significant benefits for both athletic performance and general wellness${footnote__sup__sort([ref_e1_3_footnote__sup_(ctx),ref_e1_8_footnote__sup_(ctx),ref_e1_11_footnote__sup_(ctx),ref_e1_17_footnote__sup_(ctx)])}.`].join(nl)],
 	[`## Why Choose Dr. ${mobile_hidden__span_([`Manny `])}Emenike${mobile_hidden__span_([` for EWOT`])}?`, [
 		`When you choose Dr. Manny Emenike for your EWOT journey, you can expect:`,
 		`- A comprehensive evaluation to determine if EWOT is right for you`,
@@ -64,3 +101,135 @@ export default (ctx:request_ctx_T)=>''
 		`Exercise with Oxygen Therapy (EWOT) is a safe & efficient method. To improve oxygen circulation in the body & promote overall health & wellness. With overwhelming benefits & faster results compared to other oxygen therapies. EWOT is an accessible & effective option. For individuals seeking to optimize their health & vitality.`].join(nl)],
 ])
 // @formatter:on
+function ref_e1_1_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'e1_1'
+	}, [
+		tb_a_({
+			href: 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8885311/',
+			nofollow: true,
+			wrap: true,
+		}, 'Oxygen therapy during exercise training in chronic obstructive pulmonary disease')
+	])
+}
+function ref_e1_2_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'e1_2'
+	}, [
+		tb_a_({
+			href: 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2866201/',
+			nofollow: true,
+			wrap: true,
+		}, 'Effects of oxygen on exercise duration in chronic obstructive pulmonary disease patients before and after pulmonary rehabilitation')
+	])
+}
+function ref_e1_3_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'e1_3'
+	}, [
+		tb_a_({
+			href: 'https://www.onethousandroads.com/blogs/ewot-blog/the-benefits-of-exercise-with-oxygen-therapy-ewot',
+			nofollow: true,
+			wrap: true,
+		}, 'The Benefits of Exercise with Oxygen Therapy')
+	])
+}
+function ref_e1_7_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'e1_7'
+	}, [
+		tb_a_({
+			href: 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3651238/',
+			nofollow: true,
+			wrap: true,
+		}, 'The Effects of Exercise Under Hypoxia on Cognitive Function')
+	])
+}
+function ref_e1_8_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'e1_8'
+	}, [
+		tb_a_({
+			href: 'https://www.onethousandroads.com/blogs/ewot-blog/exercise-with-oxygen-therapy-the-ultimate-guide',
+			nofollow: true,
+			wrap: true,
+		}, 'Your Guide to Exercise with Oxygen Therapy')
+	])
+}
+function ref_e1_9_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'e1_9'
+	}, [
+		tb_a_({
+			href: 'https://regenmed.io/services/ewot-exercise-with-oxygen-therapy/',
+			nofollow: true,
+			wrap: true,
+		}, 'Exercise with Oxygen Therapy (EWOT)')
+	])
+}
+function ref_e1_10_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'e1_10'
+	}, [
+		tb_a_({
+			href: 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC10662626/',
+			nofollow: true,
+			wrap: true,
+		}, 'The Impact of Exercise Training and Supplemental Oxygen on Peripheral Muscles in Chronic Obstructive Pulmonary Disease: A Randomized Controlled Trial')
+	])
+}
+function ref_e1_11_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'e1_11'
+	}, [
+		tb_a_({
+			href: 'https://physmedcolumbus.com/patients/patient-education/68-ewot-exercise-with-oxygen-therapy-benefits-and-uses',
+			nofollow: true,
+			wrap: true,
+		}, 'EWOT (Exercise with Oxygen Therapy) Benefits and Uses')
+	])
+}
+function ref_e1_15_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'e1_15'
+	}, [
+		tb_a_({
+			href: 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8650584/',
+			nofollow: true,
+			wrap: true,
+		}, 'Effects of Pre-, Post- and Intra-Exercise Hyperbaric Oxygen Therapy on Performance and Recovery: A Systematic Review and Meta-Analysis')
+	])
+}
+function ref_e1_17_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'e1_17'
+	}, [
+		tb_a_({
+			href: 'https://www.frontiersin.org/articles/10.3389/fnagi.2024.1340117/full',
+			nofollow: true,
+			wrap: true,
+		}, 'Enriched oxygen improves age-related cognitive impairment through enhancing autophagy')
+	])
+}
+function ref_e1_19_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'e1_19'
+	}, [
+		tb_a_({
+			href: 'https://openres.ersjournals.com/content/5/3/00118-2019',
+			nofollow: true,
+			wrap: true,
+		}, 'Oxygen therapy in COPD and interstitial lung disease: navigating the knowns and unknowns')
+	])
+}
