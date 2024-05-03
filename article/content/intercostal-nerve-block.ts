2e8f765bea8b8f9ceecfcc1ff7d30f762e9fb853
Brian Takita <brian.takita@gmail.com>
2024-05-03 17:46:58 -0400

    /articles/intercostal-nerve-block: + content,references

--- article/content/intercostal-nerve-block.ts
+++ article/content/intercostal-nerve-block.ts
@@ -1,40 +1,101 @@
 import { intercostal_nerve_block } from '@btakita/domain--server--peakexistence/feature'
 import { post_meta__validate } from '@rappstack/domain--server--blog/post'
+import { tb_a_ } from '@rappstack/ui--any/anchor'
 import { dl_tree_ } from '@rappstack/ui--any/dl'
-import { md__raw_ } from '@rappstack/ui--any/md'
 import { nl } from '@rappstack/ui--any/string'
+import { footnote__sup_, footnote__sup__sort } from '@rappstack/ui--server--blog/footnote'
 import { type request_ctx_T } from 'rebuildjs/server'
+import { mobile_hidden__span_ } from '../../responsive/index.js'
 import { sticky__dl_tree_props_ } from '../../sticky/index.js'
 export const meta_ = (ctx:request_ctx_T)=>post_meta__validate(ctx, {
 	pub_date: '2024-03-22T20:42:15.614Z',
-	...intercostal_nerve_block
+	...intercostal_nerve_block,
+	description: `Intercostal nerve block (ICNB) is a minimally invasive procedure. That has become a cornerstone in the management of acute & chronic pain. Associated with the chest & upper abdominal regions. By targeting the intercostal nerves, which run beneath each rib. And play a crucial role in transmitting pain signals from the chest & upper abdomen. ICNB offers significant pain relief for various conditions. This article delves into the intricacies of intercostal nerve block. Exploring its indications, procedure, benefits, & potential risks.`
 })
 // @formatter:off
 // language=md
 export default (ctx:request_ctx_T)=>''
-+ md__raw_(ctx,
-	`An Intercostal Nerve Block is a non-surgical procedure. A simple injection. It targets pain. This pain stems from the chest or upper abdominal area. Doctors use this block to diagnose or treat pain.`)
 +	dl_tree_({ ctx, _: sticky__dl_tree_props_ }, ()=>[
 		[`## What Is It?`, [
+			`An Intercostal Nerve Block is a non-surgical procedure. A simple injection. It targets pain. This pain stems from the chest or upper abdominal area. Doctors use this block to diagnose or treat pain.`,
+			``,
 			`An Intercostal Nerve Block involves local anesthetics & Corticosteroids. This medicine gets injected near the ribs. It numbs nerve pain in the chest or abdomen.`].join(nl)],
+		[`## Indications for ${mobile_hidden__span_([`Intercostal Nerve Block`, `ICNB`])}`, [
+			`ICNB is primarily indicated for pain management in conditions affecting the thorax & upper abdomen. It provides excellent analgesia for patients with rib fractures. Facilitating improved respiratory function & pain control. ICNB is beneficial for postsurgical pain management following chest & upper abdominal surgeries. Such as${footnote__sup__sort([ref_i1_1_footnote__sup_(ctx),ref_i1_2_footnote__sup_(ctx)])}:`,
+			``,
+      `- thoracotomy`,
+      `- thoracostomy`,
+      `- mastectomy`,
+      `- gastrostomy`,
+      `- cholecystectomy`,
+			``,
+      `It is also used in the treatment of herpes zoster (shingles) or post-herpetic neuralgia. Offering relief from the severe, lingering pain that sometimes follows a shingles infection${ref_i1_1_footnote__sup_(ctx)}.`].join(nl)],
 		[`## How Does It Work?`, [
 			`The block targets intercostal nerves. These nerves lie between the ribs. They send pain signals to the brain. The injection stops these signals. Thus, it reduces pain.`].join(nl)],
+		[`## The Procedure`, [
+			`The procedure for administering an intercostal nerve block. It involves placing the patient in a position that allows easy access. To the intercostal spaces. Usually lying on the side or sitting up. A healthcare provider locates the target intercostal space using ultrasound guidance. Inserts a needle either in-plane or out-of-plane. To penetrate the external & internal intercostal muscles. The optimal target is just below the internal intercostal muscle. Ensuring the needle tip remains superficial to the parietal pleura. A small volume of local anesthetic, typically 3–5 mL, is then injected at each level to achieve the block${ref_i1_1_footnote__sup_(ctx)}.`].join(nl)],
 		[`## Why Use It?`, [
 			`Patients seek this treatment for various reasons:`,
 			`- To manage chest wall pain.`,
 			`- To alleviate pain from shingles.`,
 			`- To reduce pain after surgery in the chest or upper abdomen.`].join(nl)],
-		[`## Benefits`, [
-			`- Pain relief: Effective for acute pain from rib fractures, chest wall injuries, or shingles (postherpetic neuralgia).`,
-			`- Pain management after surgery: Helps manage pain after chest or upper abdominal surgeries.`,
-			`- Diagnostic tool: Can help pinpoint the source of chronic pain.`].join(nl)],
-		[`## Risks`, [
-			`- Infection.`,
-			`- Bleeding.`,
-			`- Rarely, lung puncture.`].join(nl)],
-		[`## Conclusion`, [
-			`An Intercostal Nerve Block offers a path to relief for those battling a persistent chest or abdominal pain. It's simple & effective. Yet, it's not without risks. Discuss with your doctor.`,
+		[`## Benefits of ${mobile_hidden__span_([`Intercostal Nerve Block`, `ICNB`])}`, [
+			`The primary benefit of ICNB is its ability to provide targeted & effective pain relief. Which can significantly improve a patient's quality of life. Especially in the context of acute injuries or postoperative recovery. By alleviating pain. ICNB can facilitate better respiratory function in patients with rib fractures. reducing the risk of pulmonary complications. For surgical patients, ICNB can enhance recovery. By allowing earlier mobilization & reducing the reliance on systemic opioids. Which come with their own set of side effects${footnote__sup__sort([ref_i1_2_footnote__sup_(ctx),ref_i1_6_footnote__sup_(ctx)])}.`,
 			``,
-			`Remember, this procedure is a tool. It's part of a broader pain management strategy. Always consider the full spectrum of your health needs.`].join(nl)],
+			dl_tree_({ ctx, _: sticky__dl_tree_props_ }, ()=>[
+				[`### Pain relief`, [
+					`Effective for acute pain from rib fractures, chest wall injuries, or shingles (postherpetic neuralgia).`].join(nl)],
+				[`### Pain management after surgery`, [
+					`Helps manage pain after chest or upper abdominal surgeries.`].join(nl)],
+				[`### Diagnostic tool`, [
+					`It can help pinpoint the source of chronic pain.`].join(nl)],
+			])].join(nl)],
+		[`## Risks & Considerations`, [
+			`While ICNB is generally safe, it is not without risks. Potential complications include:`,
+			``,
+      `- pneumothorax (lung collapse)`,
+      `- local anesthetic toxicity`,
+      `- bleeding`,
+      `- infection at the injection site`,
+			``,
+      `However, these serious complications are rare. Especially when the procedure is performed using ultrasound guidance to enhance accuracy & safety. Patients may experience temporary soreness or bruising at the injection site. Which typically resolves within a few days.`].join(nl)],
+		[`## Conclusion`, [
+			`Intercostal nerve block represents a valuable tool in the arsenal of pain management strategies. Offering significant benefits for patients suffering from thoracic & upper abdominal pain. By providing targeted analgesia with minimal systemic side effects. ICNB can improve patient outcomes in both acute & chronic pain scenarios. As with any medical procedure, the decision to proceed with an ICNB should be made in consultation with a healthcare provider. Considering the specific needs & medical history of the patient. With proper application & adherence to safety protocols. ICNB can be a highly effective means of managing pain & facilitating recovery.`].join(nl)],
 	])
 // @formatter:on
+function ref_i1_1_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'i1_1'
+	}, [
+		tb_a_({
+			href: 'https://www.nysora.com/news/tips-for-an-intercostal-nerve-block-2/',
+			nofollow: true,
+			wrap: true,
+		}, 'Tips for an Intercostal Nerve Block'),
+	])
+}
+function ref_i1_2_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'i1_2'
+	}, [
+		tb_a_({
+			href: 'https://www.nysora.com/topics/regional-anesthesia-for-specific-surgical-procedures/thorax/intercostal-nerve-block/',
+			nofollow: true,
+			wrap: true,
+		}, 'Intercostal Nerve Block – Landmarks and Nerve Stimulator Technique'),
+	])
+}
+function ref_i1_6_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'i1_6'
+	}, [
+		tb_a_({
+			href: 'https://jamanetwork.com/journals/jamanetworkopen/fullarticle/2786134',
+			nofollow: true,
+			wrap: true,
+		}, 'Assessment of Intercostal Nerve Block Analgesia for Thoracic Surgery'),
+	])
+}
