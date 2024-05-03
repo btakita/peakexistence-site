7d5dd1ef284ea8ad4e1980d03716cc3211da1b56
Brian Takita <brian.takita@gmail.com>
2024-05-03 18:33:56 -0400

    /articles/joint-saving-therapy: + content,references

--- article/content/joint-saving-therapy.ts
+++ article/content/joint-saving-therapy.ts
@@ -1,13 +1,18 @@
 import { joint_saving_therapy } from '@btakita/domain--server--peakexistence/feature'
 import { post_meta__validate } from '@rappstack/domain--server--blog/post'
+import { tb_a_ } from '@rappstack/ui--any/anchor'
 import { dl_tree_ } from '@rappstack/ui--any/dl'
 import { nl } from '@rappstack/ui--any/string'
+import { footnote__sup_, footnote__sup__sort } from '@rappstack/ui--server--blog/footnote'
 import { type request_ctx_T } from 'rebuildjs/server'
+import { mobile_hidden__span_ } from '../../responsive/index.js'
 import { sticky__dl_tree_props_ } from '../../sticky/index.js'
 export const meta_ = (ctx:request_ctx_T)=>post_meta__validate(ctx, {
 	pub_date: '2024-03-22T20:42:15.614Z',
 	...joint_saving_therapy,
 	title: `Joint Saving Therapy`,
+	subtitle: `Preserving Mobility & Reducing Pain`,
+	description: `Joint saving therapy encompasses a range of treatments & strategies. Aimed at preserving joint health. Delaying the progression of joint diseases. Minimizing the need for invasive procedures like joint replacement surgery. As the prevalence of joint-related conditions such as osteoarthritis continues to rise. Particularly among aging populations. The importance of effective joint saving therapies has never been more critical. This article explores the various aspects of joint saving therapy. Including its principles, techniques, & the benefits it offers to individuals. Suffering from joint disorders.`,
 })
 // @formatter:off
 // language=md
@@ -15,9 +20,19 @@ export default (ctx:request_ctx_T)=>''
 + dl_tree_({ ctx, _: sticky__dl_tree_props_ }, ()=>[
 	[`## What is it?`, [
 		`JST encompasses various treatments. These aim to maintain or enhance joint health. It's for those with joint pain or degenerative conditions.`].join(nl)],
-	[`## Core Approaches`, [
+	[`## Principles of ${mobile_hidden__span_([`Joint Saving Therapy`, `JST`])}`, [
+		`Joint saving therapy maintains or improves joint function. While minimizing pain & preventing further damage. The approach is typically multi-faceted. Involving a combination of:`,
+		``,
+    `- physical therapy`,
+    `- lifestyle modifications`,
+    `- sometimes pharmacological interventions`,
+		``,
+    `The therapy is tailored to individual needs. Depending on the specific joint involved & the severity of the condition${footnote__sup__sort([ref_j1_1_footnote__sup_(ctx),ref_j1_2_footnote__sup_(ctx)])}.`].join(nl)],
+	[`## Key Techniques in ${mobile_hidden__span_([`Joint Saving Therapy`, `JST`])}`, [
 		dl_tree_({ ctx, _: sticky__dl_tree_props_ }, ()=>[
 			[`### Physical Therapy`, [
+				`Physical therapy is a cornerstone of joint saving therapy. It includes exercises that strengthen the muscles around the joints. Which can help stabilize & protect the joint structure. Hydrotherapy, where exercises are performed in water, can be particularly beneficial. As they reduce the load on painful joints. While still allowing for movement & resistance training${footnote__sup__sort([ref_j1_1_footnote__sup_(ctx),ref_j1_2_footnote__sup_(ctx)])}.`,
+				``,
 				dl_tree_({ ctx, _: sticky__dl_tree_props_ }, ()=>[
 					[`#### Aims`, [
 						`Improve movement, strengthen muscles.`].join(nl)],
@@ -35,6 +50,14 @@ export default (ctx:request_ctx_T)=>''
 						])
 					].join(nl)],
 				])].join(nl)],
+			[`### Pain Management`, [
+				`Effective pain management is crucial in joint saving therapy. This may involve the use of:`,
+				``,
+        `- non-steroidal anti-inflammatory drugs (NSAIDs)`,
+        `- local injections of corticosteroids`,
+        `- newer therapies such as viscosupplementation, where lubricating substances are injected into the joint`,
+				``,
+        `For chronic conditions like osteoarthritis. Managing pain is essential for maintaining mobility & quality of life${footnote__sup__sort([ref_j1_2_footnote__sup_(ctx),ref_j1_5_footnote__sup_(ctx)])}.`].join(nl)],
 			[`### Medications`, [
 				dl_tree_({ ctx, _: sticky__dl_tree_props_ }, ()=>[
 					[`#### Aims`, [
@@ -42,6 +65,29 @@ export default (ctx:request_ctx_T)=>''
 					[`#### Types`, [
 						`NSAIDs, supplements like glucosamine.`].join(nl)],
 				])].join(nl)],
+			[`### Lifestyle Changes`, [
+				`Adjustments in daily activities can significantly impact joint health. Weight management is critical. As excess body weight adds stress on weight-bearing joints like the hips & knees. Nutritional counseling may also be part of joint saving therapy. Focusing on a diet that supports joint health. Including supplements like omega-3 fatty acids which have anti-inflammatory properties${ref_j1_6_footnote__sup_(ctx)}.`,
+				``,
+				dl_tree_({ ctx, _: sticky__dl_tree_props_ }, ()=>[
+					[`#### Aims`, [
+						`Reduce joint stress.`].join(nl)],
+					[`#### How`, [
+						`Weight management, nutrition, ergonomic adjustments.`].join(nl)],
+				])].join(nl)],
+			[`### Education & Behavioral Changes`, [
+				`Educating patients about their conditions & how to protect their joints. Is another key aspect of therapy. This might include learning how to modify activities to reduce joint stress. Or using assistive devices to aid mobility. Behavioral changes such as quitting smoking or reducing alcohol consumption. It can also have a positive impact on joint health${ref_j1_6_footnote__sup_(ctx)}.`].join(nl)],
+			[`### ${mobile_hidden__span_([`Advanced `])}Non-Surgical Techniques`, [
+				`For some patients, more advanced interventions may be necessary. These can include:`,
+				``,
+        `- joint injections`,
+        `- regenerative medicine techniques like platelet-rich plasma (PRP) therapy`,
+        `- even gene therapy in some cases`,
+				``,
+        `These treatments aim to${footnote__sup__sort([ref_j1_3_footnote__sup_(ctx),ref_j1_4_footnote__sup_(ctx)])}:`,
+        ``,
+        `- reduce inflammation`,
+        `- promote healing`,
+        `- help regenerate joint tissues`].join(nl)],
 			[`### Injections`, [
 				dl_tree_({ ctx, _: sticky__dl_tree_props_ }, ()=>[
 					[`#### Aims`, [
@@ -57,15 +103,10 @@ export default (ctx:request_ctx_T)=>''
 								`An emerging technique that uses a patient's own platelets to promote healing & reduce inflammation.`].join(nl)],
 						])].join(nl)]
 				])].join(nl)],
-			[`### Lifestyle Changes`, [
-				dl_tree_({ ctx, _: sticky__dl_tree_props_ }, ()=>[
-					[`#### Aims`, [
-						`Reduce joint stress.`].join(nl)],
-					[`#### How`, [
-						`Weight management, nutrition, ergonomic adjustments.`].join(nl)],
-				])].join(nl)],
 		])].join(nl)],
-	[`## Benefits`, [
+	[`## Benefits of ${mobile_hidden__span_([`Joint Saving Therapy`, `JST`])}`, [
+		`The primary benefit of joint saving therapy is its potential to delay or even prevent the need for joint replacement surgery. By managing symptoms effectively & slowing the progression of joint damage. Patients can maintain a higher quality of life for longer. Additionally, joint saving therapy can help individuals remain active. Which is crucial for overall health & well-being.`,
+		``,
 		dl_tree_({ ctx, _: sticky__dl_tree_props_ }, ()=>[
 			[`### Pain Reduction`, [
 				`Alleviates discomfort.`].join(nl)],
@@ -78,8 +119,8 @@ export default (ctx:request_ctx_T)=>''
 		dl_tree_({ ctx, _: sticky__dl_tree_props_ }, ()=>[
 			[`### Pros`, [
 				`- Non-surgical, less risk.`,
-				`- Can improve joint health naturally.`,
-				`- Offers comprehensive, holistic approach.`,
+				`- It can improve joint health naturally.`,
+				`- Offers a comprehensive, holistic approach.`,
 			].join(nl)],
 			[`### Cons`, [
 				`- May not offer permanent solutions.`,
@@ -88,6 +129,84 @@ export default (ctx:request_ctx_T)=>''
 			].join(nl)],
 		])].join(nl)],
 	[`## Conclusion`, [
-		`Joint Saving Therapy offers hope. It's for those seeking to avoid joint replacement. It combines treatments to enhance joint health & mobility. Yet, it's not a one-size-fits-all. Discuss options with your healthcare provider. It's a journey toward maintaining your body's natural joints for as long as possible.`].join(nl)],
+		`Joint saving therapy offers a comprehensive approach to managing joint health. Combining physical interventions, lifestyle changes, & sometimes advanced medical treatments. Its goal is to:`,
+		``,
+    `- preserve joint function`,
+    `- minimize pain`,
+    `- delay the progression of joint diseases`,
+		``,
+    `As research continues to advance, new & more effective joint saving therapies. Are likely to become available. Offering hope & improved outcomes. For people suffering from joint-related conditions.`].join(nl)],
 ])
 // @formatter:on
+function ref_j1_1_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'j1_1'
+	}, [
+		tb_a_({
+			href: 'https://www.ehlers-danlos.org/information/physical-therapy-for-hypermobility/',
+			nofollow: true,
+			wrap: true,
+		}, 'Physical therapy for hypermobility'),
+	])
+}
+function ref_j1_2_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'j1_2'
+	}, [
+		tb_a_({
+			href: 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7990728/',
+			nofollow: true,
+			wrap: true,
+		}, 'Knee osteoarthritis: key treatments and implications for physical therapy'),
+	])
+}
+function ref_j1_3_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'j1_3'
+	}, [
+		tb_a_({
+			href: 'https://prime-pt.com/joint-mobilization',
+			nofollow: true,
+			wrap: true,
+		}, 'Joint Mobilization Treatment in Lee\'s Summit, MO'),
+	])
+}
+function ref_j1_4_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'j1_4'
+	}, [
+		tb_a_({
+			href: 'https://www.amazon.com/Penetrex-Joint-Muscle-Therapy-Non-Greasy/dp/B00DI7U88G',
+			nofollow: true,
+			wrap: true,
+		}, 'Penetrex Joint & Muscle Therapy – Soothing Comfort for Back, Neck, Hands, Feet – Premium Whole Body Rub with Arnica, Vitamin B6 MSM & Boswellia – Non-Greasy 4oz Cream'),
+	])
+}
+function ref_j1_5_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'j1_5'
+	}, [
+		tb_a_({
+			href: 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2826784/',
+			nofollow: true,
+			wrap: true,
+		}, 'Management of aromatase inhibitor–induced arthralgia'),
+	])
+}
+function ref_j1_6_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'j1_6'
+	}, [
+		tb_a_({
+			href: 'https://www.arthritis.org/health-wellness/healthy-living/managing-pain/joint-protection/16-joint-protection-tips',
+			nofollow: true,
+			wrap: true,
+		}, '16 Joint-Protection Tips'),
+	])
+}
