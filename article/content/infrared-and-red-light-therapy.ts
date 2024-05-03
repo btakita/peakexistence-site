9c8395af014a4c9f5a31d7aa6da87878f36ecacb
Brian Takita <brian.takita@gmail.com>
2024-05-03 17:15:47 -0400

    /articles/infrared-and-red-light-therapy: + content,references

--- article/content/infrared-and-red-light-therapy.ts
+++ article/content/infrared-and-red-light-therapy.ts
@@ -1,20 +1,32 @@
 import { infrared_and_red_light_therapy } from '@btakita/domain--server--peakexistence/feature'
 import { post_meta__validate } from '@rappstack/domain--server--blog/post'
+import { tb_a_ } from '@rappstack/ui--any/anchor'
 import { dl_tree_ } from '@rappstack/ui--any/dl'
 import { nl } from '@rappstack/ui--any/string'
+import { footnote__sup_, footnote__sup__sort } from '@rappstack/ui--server--blog/footnote'
+import { class_ } from 'ctx-core/html'
 import { type request_ctx_T } from 'rebuildjs/server'
 import { mobile_hidden__span_ } from '../../responsive/index.js'
 import { sticky__dl_tree_props_ } from '../../sticky/index.js'
+const description_md = [
+	`The sun's warmth on your skin is a comforting sensation. Red light therapy treatments harness specific wavelengths of light. To penetrate the skin, aiming to rejuvenate, heal, & energize at a cellular level. Infrared & red light therapy, collectively known as photobiomodulation (PBM), are non-invasive treatments that use specific wavelengths of light to penetrate the skin & promote cellular repair & regeneration. These therapies have gained significant attention in the medical community for their ability to alleviate pain, reduce inflammation, enhance skin health, & accelerate healing.`,
+	``,
+	`This article explores the mechanisms, benefits, & applications of infrared & red light therapy. Providing insights into how these light-based treatments are transforming approaches. To health & wellness.`
+].join(nl)
 export const meta_ = (ctx:request_ctx_T)=>post_meta__validate(ctx, {
 	pub_date: '2024-03-22T20:42:15.614Z',
 	...infrared_and_red_light_therapy,
+	description: description_md,
+	description_md
 })
 // @formatter:off
 // language=md
 export default (ctx:request_ctx_T)=>''
 + dl_tree_({ ctx, _: sticky__dl_tree_props_ }, ()=>[
-	[`## ${mobile_hidden__span_([`What is `])}Infrared & Red Light Therapy?`, [
-		`Infrared & red light therapy use low levels of red light & infrared light. To stimulate cellular repair & regeneration. Infrared & red light therapy are forms of photobiomodulation. Using light to stimulate cellular activity. Leading to beneficial effects on the body's tissues. Unlike ultraviolet light, these therapies provide healing energy without adverse effects.`,
+	[`## ${mobile_hidden__span_([`What is `])}Infrared & Red Light Therapy${mobile_hidden__span_([`?`])}`, [
+		`Infrared & red light therapy uses low levels of red light & infrared light. To stimulate cellular repair & regeneration. Infrared & red light therapy are forms of photobiomodulation. Using light to stimulate cellular activity. Leading to beneficial effects on the body's tissues. Unlike ultraviolet light, these therapies provide healing energy without adverse effects.`,
+		``,
+		`Infrared & red light therapy involves the use of light-emitting diodes (LEDs) or lasers. NTto deliver specific wavelengths of light to the body. Red light ranges from 620 to 700 nanometers (nm), while infrared light spans from 700 nm to about 1,000 nm. These wavelengths penetrate different depths of tissue. With red light affecting surface layers such as the skin. With infrared light reaching deeper tissues like muscles & joints${footnote__sup__sort([ref_i1_3_footnote__sup_(ctx),ref_i1_15_footnote__sup_(ctx)])}.`,
 		``,
 		`The mechanism behind PBM uses the light's interaction with mitochondria. The energy-producing centers within cells. This interaction boosts the production of adenosine triphosphate (ATP). Enhancing cellular energy & promoting various healing processes. PBM is used in various medical fields, including:`,
 		`- dentistry`,
@@ -22,7 +34,25 @@ export default (ctx:request_ctx_T)=>''
 		`- physical therapy`,
 		`- pain management`,
 		``,
-		`PBM accelerates recovery & relieve symptoms without invasive procedures.`].join(nl)],
+		`PBM accelerates recovery & relieves symptoms without invasive procedures.`].join(nl)],
+	[`## Mechanisms of Action`, [
+		`The therapeutic effects of infrared & red light therapy. Are primarily attributed to their impact on mitochondrial function within cells. The mitochondria are the "powerhouses" of cells, responsible for energy production. Red & infrared light is absorbed by a mitochondrial enzyme called cytochrome c oxidase. Which plays a pivotal role in the cellular respiratory chain${footnote__sup__sort(ref_i1_7_footnote__sup_(ctx),ref_i1_8_footnote__sup_(ctx))}.`,
+		``,
+		`This absorption of light energy leads to:`,
+		``,
+		dl_tree_({ ctx, _: sticky__dl_tree_props_ }, ()=>[
+			[`### Increased Production of ATP`, [
+				`Enhanced ATP (adenosine triphosphate) production provides more energy for cells. Facilitating various healing & regenerative processes${footnote__sup__sort(ref_i1_7_footnote__sup_(ctx),ref_i1_8_footnote__sup_(ctx))}.`].join(nl)],
+			[`### Release of Nitric Oxide`, [
+				`Light therapy stimulates the release of nitric oxide. A vasodilator that improves blood circulation. To deliver more oxygen & nutrients to tissues. Which aids in healing & reduces pain${footnote__sup__sort([ref_i1_3_footnote__sup_(ctx),ref_i1_15_footnote__sup_(ctx)])}.`].join(nl)],
+			[`### Reduction in Oxidative Stress`, [
+				`By increasing cellular energy production, light therapy helps reduce oxidative stress. Oxidative stress causes${footnote__sup__sort(ref_i1_7_footnote__sup_(ctx),ref_i1_8_footnote__sup_(ctx))}:`,
+				``,
+        `- inflammation`,
+        `- aging`,
+        `- chronic disease processes`].join(nl)],
+		]),
+	].join(nl)],
 	[`## How They Work`, [
 		dl_tree_({ ctx, _: sticky__dl_tree_props_ }, ()=>[
 			[`### Red Light Therapy (RLT)`, [
@@ -33,18 +63,31 @@ export default (ctx:request_ctx_T)=>''
 				`- relieving pain`,
 				`- enhancing muscle recovery`].join(nl)]
 		])].join(nl)],
-	[`## Benefits Unveiled`, [
+	[`## Clinical Applications${mobile_hidden__span_([` & `, `/`])}Benefits`, [
 		dl_tree_({ ctx, _: sticky__dl_tree_props_ }, ()=>[
-			[`### Skin Rejuvenation`, [
-				`Promotes collagen production, reducing wrinkles & improving skin tone.`].join(nl)],
-			[`### Pain Relief`, [
-				`Eases joint & muscle pain by reducing inflammation & increasing circulation.`].join(nl)],
+			[`### Pain Management`, [
+				`Infrared & red light therapy is effective in reducing pain associated with various conditions. Including arthritis, back pain, & tendinitis. By enhancing circulation & reducing inflammation. These therapies help alleviate pain & improve mobility${footnote__sup__sort([ref_i1_3_footnote__sup_(ctx),ref_i1_15_footnote__sup_(ctx)])}.`].join(nl)],
+			[`### Skin Health & Rejuvenation`, [
+				`Red light therapy is particularly noted for its anti-aging effects on the skin. It${footnote__sup__sort([ref_i1_11_footnote__sup_(ctx),ref_i1_13_footnote__sup_(ctx)])}:`,
+				``,
+        `- stimulates collagen production`,
+        `- reduces wrinkles`,
+        `- improves skin elasticity`,
+        `- accelerates wound healing`,
+        `- reduces scarring`,
+        `- reduces signs of UV damage`].join(nl)],
+			[`### Accelerated Healing`, [
+				`Both therapies promote faster healing of wounds, burns, & surgical incisions. By stimulating tissue regeneration & reducing inflammation. This makes them valuable tools in post-operative care & injury recovery${footnote__sup__sort([ref_i1_1_footnote__sup_(ctx),ref_i1_2_footnote__sup_(ctx)])}.`].join(nl)],
 			[`### Improved Muscle Recovery`, [
 				`Accelerates the healing process in muscles, making it popular among athletes.`].join(nl)],
 			[`### Enhanced Circulation`, [
 				`Boosts blood flow, delivering more oxygen & nutrients to tissues.`].join(nl)],
+			[`### Mental Well-being`, [
+				`Emerging research suggests that red light therapy has beneficial effects on mental health. Including reducing symptoms of depression & anxiety. The therapy's ability to improve cellular function extends to neurological benefits. Enhancing mood & cognitive function${ref_i1_15_footnote__sup_(ctx)}.`].join(nl)],
 			[`### Mood Enhancement`, [
 				`Exposure to red & infrared light can improve mood & energy. By increasing serotonin levels.`].join(nl)],
+			[`### Safety & Considerations`, [
+				`Infrared & red light therapy is generally considered safe, with minimal side effects. It is crucial to use these therapies under the guidance of a qualified professional. especially when treating serious medical conditions. The most common side effects include temporary discomfort & redness at the treatment site${ref_i1_14_footnote__sup_(ctx)}.`].join(nl)],
 		])].join(nl)],
 	[`## Considerations`, [
 		dl_tree_({ ctx, _: sticky__dl_tree_props_ }, ()=>[
@@ -55,7 +98,7 @@ export default (ctx:request_ctx_T)=>''
 			].join(nl)],
 			[`### Cons`, [
 				`- Results can vary & may take time to manifest`,
-				`- Initial cost of devices or therapy sessions can be high`,
+				`- The initial cost of devices or therapy sessions can be high`,
 				`- Not a standalone cure for serious medical conditions`,
 			].join(nl)],
 		])].join(nl)],
@@ -65,10 +108,128 @@ export default (ctx:request_ctx_T)=>''
 		`- individuals with chronic pain conditions`,
 		`- those seeking anti-aging solutions`].join(nl)],
 	[`## Getting Started`, [
-		`1. **Research:** Understand the specific benefits & find a reputable provider or device.`,
-		`2. **Consultation:** Talk to a healthcare professional, especially if you have pre-existing conditions.`,
-		`3. **Trial Sessions:** Start with a few sessions to see how your body responds.`].join(nl)],
+		dl_tree_({ ctx, _: sticky__dl_tree_props_,
+			dl_class: '[counter-reset:_order]',
+			dt_class: class_('[&>h3]:before:[counter-increment:order]', '[&>h3]:before:content-[counter(order)_\'._\']')
+		}, ()=>[
+			[`### Research`, [
+				`Understand the specific benefits & find a reputable provider or device.`].join(nl)],
+			[`### Consultation`, [
+				`Talk to a healthcare professional, especially if you have pre-existing conditions.`].join(nl)],
+			[`### Trial Sessions`, [
+				`Start with a few sessions to see how your body responds.`].join(nl)],
+		])].join(nl)],
+	[`## Takeaway`, [
+		`Infrared & red light therapy represents a significant advancement in non-invasive medical treatments. Offering a wide range of health benefits from pain relief & injury recovery. To skin rejuvenation & mental health improvement. These therapies are likely to become more integrated into routine medical practice. Providing patients with safe, effective, & natural treatment options for a variety of ailments${footnote__sup__sort([ref_i1_3_footnote__sup_(ctx),ref_i1_15_footnote__sup_(ctx)])}.`].join(nl)],
 	[`## Conclusion`, [
-		`Infrared & red light therapy cast a warm glow on non-invasive healing techniques. By offering a spectrum of health benefits with minimal risks. They are an example of using light as medicine. Solace from pain, yearning for youthful skin, or striving for peak physical performance. These therapies bring wellness illuminated by the very essence of light.`].join(nl)],
+		`Infrared & red light therapy casts a warm glow on non-invasive healing techniques. By offering a spectrum of health benefits with minimal risks. They are an example of using light as medicine. Solace from pain, yearning for youthful skin, or striving for peak physical performance. These therapies bring wellness illuminated by the very essence of light.`].join(nl)],
 ])
 // @formatter:on
+function ref_i1_1_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'i1_1'
+	}, [
+		tb_a_({
+			href: 'https://pubmed.ncbi.nlm.nih.gov/27943458/',
+			nofollow: true,
+			wrap: true,
+		}, 'Noninvasive red and near-infrared wavelength-induced photobiomodulation: promoting impaired cutaneous wound healing'),
+	])
+}
+function ref_i1_2_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'i1_2'
+	}, [
+		tb_a_({
+			href: 'https://www.degreewellness.com/2020/05/red-light-therapy-for-healing-injuries-wounds-scars-and-burns/',
+			nofollow: true,
+			wrap: true,
+		}, 'Red Light Therapy for Healing Injuries, Wounds, Scars, and Burns'),
+	])
+}
+function ref_i1_3_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'i1_3'
+	}, [
+		tb_a_({
+			href: 'https://www.celluma.com/pages/light-therapy-pain-management',
+			nofollow: true,
+			wrap: true,
+		}, 'Celluma: Best Infrared Light Therapy for Pain'),
+	])
+}
+function ref_i1_7_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'i1_7'
+	}, [
+		tb_a_({
+			href: 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4148276/',
+			nofollow: true,
+			wrap: true,
+		}, 'Effects of low-power light therapy on wound healing: LASER x LED*'),
+	])
+}
+function ref_i1_8_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'i1_8'
+	}, [
+		tb_a_({
+			href: 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8277709/',
+			nofollow: true,
+			wrap: true,
+		}, 'Mechanisms and Pathways of Pain Photobiomodulation: A Narrative Review'),
+	])
+}
+function ref_i1_11_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'i1_11'
+	}, [
+		tb_a_({
+			href: 'https://www.cnn.com/cnn-underscored/beauty/what-is-red-light-therapy',
+			nofollow: true,
+			wrap: true,
+		}, 'What is red light therapy for skin? Experts explain the anti-aging benefits'),
+	])
+}
+function ref_i1_13_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'i1_13'
+	}, [
+		tb_a_({
+			href: 'https://www.degreewellness.com/2019/10/treating-inflammation-and-pain-with-red-and-near-infrared-light-therapy/',
+			nofollow: true,
+			wrap: true,
+		}, 'Treating Inflammation and Pain with Red and Near Infrared Light Therapy'),
+	])
+}
+function ref_i1_14_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'i1_14'
+	}, [
+		tb_a_({
+			href: 'https://www.webmd.com/skin-problems-and-treatments/red-light-therapy',
+			nofollow: true,
+			wrap: true,
+		}, 'What Is Red Light Therapy?'),
+	])
+}
+function ref_i1_15_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'i1_15'
+	}, [
+		tb_a_({
+			href: 'https://vitalredlight.com/red-light-therapy-for-mental-health/',
+			nofollow: true,
+			wrap: true,
+		}, 'Red light therapy for mental health and wellbeing'),
+	])
+}
