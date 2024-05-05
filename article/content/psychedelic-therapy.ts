b003173213bb3cb46e3ed73902af622c6cea153f
Brian Takita <brian.takita@gmail.com>
2024-05-04 23:33:06 -0400

    /articles/psychedelic-therapy: + content,references

--- article/content/psychedelic-therapy.ts
+++ article/content/psychedelic-therapy.ts
@@ -1,37 +1,109 @@
 import { psychedelic_therapy } from '@btakita/domain--server--peakexistence/feature'
 import { post_meta__validate } from '@rappstack/domain--server--blog/post'
+import { tb_a_ } from '@rappstack/ui--any/anchor'
 import { dl_tree_ } from '@rappstack/ui--any/dl'
 import { nl } from '@rappstack/ui--any/string'
+import { footnote__sup_, footnote__sup__sort } from '@rappstack/ui--server--blog/footnote'
 import { type request_ctx_T } from 'rebuildjs/server'
 import { mobile_hidden__span_ } from '../../responsive/index.js'
 import { sticky__dl_tree_props_ } from '../../sticky/index.js'
 export const meta_ = (ctx:request_ctx_T)=>post_meta__validate(ctx, {
 	pub_date: '2024-03-22T20:42:15.614Z',
 	...psychedelic_therapy,
+	description: `Psychedelic therapy, an innovative approach in the field of psychiatry. Involves the use of psychedelic substances like psilocybin, LSD, MDMA, & ayahuasca. To facilitate profound psychological healing & growth. This therapeutic method combines controlled doses of psychedelics with psychotherapy. Is gaining traction as a potential treatment for a variety of mental health disorders. Including depression, PTSD, & addiction. This article explores the mechanisms, benefits, & current research landscape of psychedelic therapy.`,
+	description_md: [
+		`Psychedelic & Exercise Therapy leverages the psychological effects of psychedelics & the benefits of exercise. To foster a holistic sense of wellness. Opening new avenues for:`,
+		``,
+		`- enhancing mental acuity`,
+		`- emotional well-being`,
+		`- physical performance`].join(nl)
 })
 // @formatter:off
 // language=md
 export default (ctx:request_ctx_T)=>''
 + dl_tree_({ ctx, _: sticky__dl_tree_props_ }, ()=>[
-	[`## ${mobile_hidden__span_([`What is `])}Psychedelic Therapy?`, [
-		`Psychedelic Exercise Therapy combines physical activity & carefully administered psychedelic substances. To improve mental health & overall well-being. Dr. Manny Emenike addresses the mind & body for comprehensive healing. As a medical doctor specializing in internal medicine.`,
+	[`## ${mobile_hidden__span_([`Understanding `])}Psychedelic Therapy`, [
+		`Psychedelic therapy involves the controlled use of substances like psilocybin, DMT, peyote, ayahuasca, ibogaine, ketamine, MDMA, & LSD.`,
 		``,
-		`Psychedelic therapy is also known as psychedelic-assisted psychotherapy (PAP).  Psychedelic therapy is a psychiatric practice. Involving the controlled use of psychedelic substances as part of a psychotherapeutic process.`,
+		`To facilitate deep psychological insights & emotional healing. Clinical research demonstrates the potential of psychedelics to treat mental health conditions including${footnote__sup__sort([ref_p2_2_footnote__sup_(ctx),ref_p2_4_footnote__sup_(ctx)])}:`,
 		``,
-		`Psychedelic therapy combines the ingestion of consciousness-altering psychedelics. With talk therapy to address various mental health conditions & promote therapeutic benefits. Several psychedelics are under research for their therapeutic potential. In clinical & nonclinical settings.`,
+    `- depression`,
+    `- anxiety`,
+    `- PTSD`,
+    `- substance use disorders`,
 		``,
-		`Including, plant-derived:`,
-		`- psilocybin`,
-		`- DMT`,
-		`- peyote`,
-		`- ayahuasca`,
-		`- ibogaine`,
+		dl_tree_({ ctx, _: sticky__dl_tree_props_ }, ()=>[
+			[`### Benefits`, [
+				`By promoting neuroplasticity & altering consciousness. In a way that can lead to significant therapeutic breakthroughs. Psychedelic therapy typically involves one to a few dosing sessions. In conjunction with psychotherapy sessions. These substances induce altered states of consciousness that can lead to${footnote__sup__sort([ref_p1_5_footnote__sup_(ctx),ref_p1_8_footnote__sup_(ctx)])}:`,
+				``,
+				dl_tree_({ ctx, _: sticky__dl_tree_props_ }, ()=>[
+					[`### Rapid Symptom Relief`, [
+						`Psychedelic therapy often produces quick & lasting improvements in symptoms. Unlike many traditional treatments that can take weeks to become effective.`].join(nl)],
+					[`### ${mobile_hidden__span_([`Enhanced `])}Self-awareness${mobile_hidden__span_([` & `, `/`])}Emotional Processing`, [
+						`Patients often report increased self-awareness & emotional processing. Which is critical for long-term psychological health.`].join(nl)],
+					[`### ${mobile_hidden__span_([`Reduction in `])}Treatment-Resistant Symptoms`, [
+						`Psychedelic therapy has been particularly effective in treating symptoms. That has not responded to other treatment modalities.`].join(nl)],
+				])].join(nl)],
+			[`### Mechanisms of Action`, [
+				`The therapeutic effects of psychedelics stem from altering brain function. Research suggests that psychedelics disrupt the default mode network (DMN). A brain network associated with self-referential thoughts & the ego. This disruption increases brain connectivity & plasticity. Allowing patients to break free from rigid patterns of thought & behavior${footnote__sup__sort([ref_p1_12_footnote__sup_(ctx),ref_p1_15_footnote__sup_(ctx)])}.`,
+				``,
+				`Psychedelics enhance emotional openness & introspection. Which can lead to profound insights & emotional catharsis during therapy sessions. This can be particularly beneficial for patients dealing with deeply rooted psychological issues${footnote__sup__sort([ref_p1_5_footnote__sup_(ctx),ref_p1_8_footnote__sup_(ctx)])}.`,
+				``,
+				dl_tree_({ ctx, _: sticky__dl_tree_props_ }, ()=>[
+					[`#### Enhanced Cognitive Flexibility`, [
+						`Psychedelics such as psilocybin & LSD temporarily disrupt the brain's default mode network (DMN). Which is involved in self-referential thoughts & maintaining one's sense of ego or identity${ref_p1_12_footnote__sup_(ctx)}. By reducing the activity of the DMN, psychedelics increase cognitive flexibility. Allowing individuals to think in more creative & less rigid ways. This can lead to new insights into personal & interpersonal issues. Which are often inaccessible under normal states of consciousness.`].join(nl)],
+					[`#### Increased Emotional Connectivity`, [
+						`Psychedelics can enhance emotional connectivity & empathy. Making it easier for individuals to access & process emotions linked to their problems${footnote__sup__sort([ref_p1_12_footnote__sup_(ctx),ref_p3_3_footnote__sup_(ctx)])}. This emotional processing can lead to a more profound understanding of the root causes of their issues. And foster a greater sense of connection with others. Which is often therapeutic.`].join(nl)],
+					[`#### Shift in Perspective`, [
+						`Altered states of consciousness can lead to experiences described as mystical or transcendent. These experiences can change the way individuals perceive their problems, including${footnote__sup__sort([ref_p3_2_footnote__sup_(ctx),ref_p3_4_footnote__sup_(ctx)])}:`,
+						``,
+            `- reducing the perceived magnitude of issues`,
+            `- enhancing their sense of meaning & purpose in life`,
+						``,
+            `Such shifts can be particularly beneficial for individuals dealing with existential distress. Or those who feel stuck in negative thought cycles.`].join(nl)],
+				])
+			].join(nl)],
+			[`### Clinical Research & Evidence`, [
+				`Recent studies show promising results for psychedelic therapy. In treating various mental health conditions. Research on psilocybin has demonstrated significant improvements in symptoms of depression & anxiety. Particularly in individuals with life-threatening cancer. Highlighting its potential to alleviate existential distress${footnote__sup__sort([ref_p1_5_footnote__sup_(ctx),ref_p1_9_footnote__sup_(ctx)])}. MDMA-assisted therapy has shown efficacy in treating PTSD. With patients experiencing considerable reductions in symptoms after just a few sessions${footnote__sup__sort([ref_p1_8_footnote__sup_(ctx),ref_p1_18_footnote__sup_(ctx)])}.`,
+				``,
+				`Clinical trials have recognized the safety & efficacy of these treatments. Leading to breakthrough therapy designations by the FDA for psilocybin & MDMA. For specific conditions. This designation expedites the development & review of drugs. That shows the potential to improve treatment for serious conditions${footnote__sup__sort([ref_p1_5_footnote__sup_(ctx),ref_p1_8_footnote__sup_(ctx)])}.`,
+				``,
+				dl_tree_({ ctx, _: sticky__dl_tree_props_ }, ()=>[
+					[`#### Depression & Anxiety`, [
+						`Patients often report reductions in symptoms of depression & anxiety following psychedelic therapy. As they are able to view their life's challenges from a broader, more detached perspective. This can diminish the overwhelming nature of such challenges. And increase individuals' confidence in their ability to overcome them${footnote__sup__sort([ref_p1_12_footnote__sup_(ctx),ref_p3_2_footnote__sup_(ctx)])}.`].join(nl)],
+					[`#### PTSD`, [
+						`Psychedelic therapy can help individuals with PTSD reframe & process traumatic memories. In a safe & controlled environment. **Psychedelic-altered perception can reduce the emotional impact of traumatic memories.** Making them less distressing & easier to integrate${ref_p3_3_footnote__sup_(ctx)}.`].join(nl)],
+					[`#### Addiction`, [
+						`Psychedelic therapy can help individuals understand the underlying emotional & psychological drivers. Of addictive behaviors. By altering their perception of these problems. Individuals may find new motivations & pathways to recovery. That was previously obscured by habitual patterns of behavior${footnote__sup__sort([ref_p3_3_footnote__sup_(ctx),ref_p3_4_footnote__sup_(ctx)])}.`].join(nl)],
+				])
+			].join(nl)],
+		])
+	].join(nl)],
+	[`## Exercise Therapy${mobile_hidden__span_([`: Enhancing Physical & Mental Health`])}`, [
+		`Exercise therapy encompasses structured physical activities designed to achieve specific health outcomes. Beyond its physical benefits, exercise is known to${footnote__sup__sort([ref_p2_12_footnote__sup_(ctx)/*,ref_p2_15_footnote__sup_(ctx)*/])}:`,
 		``,
-		`And chemical compounds:`,
-		`- ketamine`,
-		`- MDMA`,
-		`- LSD`].join(nl)],
-	[`## Why Choose Psychedelic Therapy?`, [
+    `- improve mood`,
+    `- reduce anxiety`,
+    `- depression symptoms`,
+    `- enhance cognitive function`,
+		``,
+    `Making it a valuable tool for mental health management.`].join(nl)],
+	[`## ${mobile_hidden__span_([`Synergistic Effects of `])}Psychedelics & Exercise`, [
+		`Anecdotal evidence & preliminary research. It suggests that combining psychedelics with exercise amplifies the benefits of both therapies. Psychedelics increase mindfulness, sensory perception, & appreciation of the natural world. Which could enhance the enjoyment & psychological benefits of exercise. The mood-lifting & neuroplastic effects of psychedelics may complement the endorphin release & cognitive benefits. Associated with regular physical activity. Leading to improved exercise performance & greater overall well-being${footnote__sup__sort([ref_p2_1_footnote__sup_(ctx),ref_p2_5_footnote__sup_(ctx)])}.`].join(nl)],
+	[`## Potential Benefits & Considerations`, [
+		`Psychedelics with exercise therapy address both mental & physical health. Benefits include${footnote__sup__sort([ref_p2_3_footnote__sup_(ctx),ref_p2_4_footnote__sup_(ctx)])}:`,
+		``,
+    `- enhanced exercise motivation`,
+    `- improved mood & mental health`,
+    `- increased creativity & problem-solving during physical activities`,
+    `- deeper, more meaningful engagement with exercise routines`,
+		``,
+		`This approach also presents challenges & considerations. Significant barriers include:`,
+		``,
+    `- the legal status of psychedelics`,
+    `- variability in individual responses to these substances`,
+    `- the need for professional guidance to safely navigate psychedelic experiences`].join(nl)],
+	[`## Psychedelic Therapy with Peak Existence`, [
 		dl_tree_({ ctx, _: sticky__dl_tree_props_ }, ()=>[
 			[`### Holistic Healing`, [
 				`Dr. Emenike treats the root causes of ailments with treating symptoms. Combining exercise & psychedelic therapy addresses mental, emotional, & physical health. Promoting a truly holistic healing experience`].join(nl)],
@@ -45,7 +117,200 @@ export default (ctx:request_ctx_T)=>''
 				``,
 				`Dr. Emenike maintains a patient-centric approach, emphasizing collaboration & understanding.`].join(nl)],
 			[`### Cutting-edge Research`, [
-				`Dr. Emenike continually integrates evidence-based research & best practices into his treatment protocols. Offering patients the most effective & up-to-date care. Dr. Emenike's brings expert discresion with avid interest in the latest scientific advancements. Bringing the best up-to-date care to you.`].join(nl)],
+				`Dr. Emenike continually integrates evidence-based research & best practices into his treatment protocols. Offering patients the most effective & up to date care. Dr. Emenike's brings expert discresion with avid interest in the latest scientific advancements. Bringing the best up-to-date care to you.`].join(nl)],
 		])].join(nl)],
 ])
 // @formatter:on
+function ref_p1_5_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'p1_5'
+	}, [
+		tb_a_({
+			href: 'https://en.wikipedia.org/wiki/Psychedelic_therapy',
+			nofollow: true,
+			wrap: true,
+		}, 'Psychedelic therapy (Wikipedia)')
+	])
+}
+function ref_p1_8_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'p1_8'
+	}, [
+		tb_a_({
+			href: 'https://www.healthline.com/health/mental-health/psychedelic-therapy',
+			nofollow: true,
+			wrap: true,
+		}, 'Psychedelic Therapy Is Having a Moment — Here’s What You Need to Know')
+	])
+}
+function ref_p1_9_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'p1_9'
+	}, [
+		tb_a_({
+			href: 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC9226617/',
+			nofollow: true,
+			wrap: true,
+		}, 'Psychedelic-Assisted Psychotherapy—A Systematic Review of Associated Psychological Interventions')
+	])
+}
+function ref_p1_12_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'p1_12'
+	}, [
+		tb_a_({
+			href: 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC9553847/',
+			nofollow: true,
+			wrap: true,
+		}, 'The Emerging Field of Psychedelic Psychotherapy')
+	])
+}
+function ref_p1_15_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'p1_15'
+	}, [
+		tb_a_({
+			href: 'https://www.webmd.com/mental-health/story/how-do-psychedelic-treatments-work',
+			nofollow: true,
+			wrap: true,
+		}, 'How Do Psychedelic Treatments Work?')
+	])
+}
+function ref_p1_18_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'p1_18'
+	}, [
+		tb_a_({
+			href: 'https://time.com/6253702/psychedelics-psilocybin-mdma-legalization/',
+			nofollow: true,
+			wrap: true,
+		}, 'Psychedelics May Be Part of U.S. Medicine Sooner Than You Think')
+	])
+}
+function ref_p2_1_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'p2_1'
+	}, [
+		tb_a_({
+			href: 'https://satisfyrunning.com/blogs/possessed/pyschedelics-improve-athletic-performance',
+			nofollow: true,
+			wrap: true,
+		}, 'Do Pyschedelics Improve Athletic Performance?')
+	])
+}
+function ref_p2_2_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'p2_2'
+	}, [
+		tb_a_({
+			href: 'https://www.pharmaceutical-technology.com/features/psychedelic-treatments-for-mental-health-is-there-substance-behind-the-hype/',
+			nofollow: true,
+			wrap: true,
+		}, 'Psychedelic treatments for mental health: Is there substance behind the hype?')
+	])
+}
+function ref_p2_3_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'p2_3'
+	}, [
+		tb_a_({
+			href: 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8801670/',
+			nofollow: true,
+			wrap: true,
+		}, 'Psychedelics and health behaviour change')
+	])
+}
+function ref_p2_4_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'p2_4'
+	}, [
+		tb_a_({
+			href: 'https://jamanetwork.com/journals/jama/fullarticle/2808951',
+			nofollow: true,
+			wrap: true,
+		}, 'Psychedelic Therapy—A New Paradigm of Care for Mental Health')
+	])
+}
+function ref_p2_5_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'p2_5'
+	}, [
+		tb_a_({
+			href: 'https://journalofexerciseandnutrition.com/index.php/JEN/article/view/50',
+			nofollow: true,
+			wrap: true,
+		}, 'Psychedelics: Overlooked Clinical Tools with Unexplored Ergogenic Potential')
+	])
+}
+function ref_p2_12_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'p2_12'
+	}, [
+		tb_a_({
+			href: 'https://link.springer.com/article/10.1007/s10517-013-2213-2',
+			nofollow: true,
+			wrap: true,
+		}, 'Effects of Drugs on Exercise Performance')
+	])
+}
+/*404*/
+// function ref_p2_15_footnote__sup_(ctx:request_ctx_T) {
+// 	return footnote__sup_({
+// 		ctx,
+// 		id: 'p2_15'
+// 	}, [
+// 		tb_a_({
+// 			href: 'https://morningchalkup.com/community/2022/07/27/how-microdosing-shrooms-increases-exercise-fatigue-and-why-you-should-do-them-anyway/',
+// 			nofollow: true,
+// 			wrap: true,
+// 		}, 'How Microdosing Shrooms Increases Exercise Fatigue (And Why You Should Do Them Anyway)')
+// 	])
+// }
+function ref_p3_2_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'p3_2'
+	}, [
+		tb_a_({
+			href: 'https://pubs.acs.org/doi/10.1021/acsptsci.0c00194',
+			nofollow: true,
+			wrap: true,
+		}, 'The Subjective Effects of Psychedelics Are Necessary for Their Enduring Therapeutic Effects')
+	])
+}
+function ref_p3_3_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'p3_3'
+	}, [
+		tb_a_({
+			href: 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4592297/',
+			nofollow: true,
+			wrap: true,
+		}, 'Psychedelic medicine: a re-emerging therapeutic paradigm')
+	])
+}
+function ref_p3_4_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'p3_4'
+	}, [
+		tb_a_({
+			href: 'https://www.medicalnewstoday.com/articles/psychedelics',
+			nofollow: true,
+			wrap: true,
+		}, 'What are psychedelics and what can they do?')
+	])
+}
