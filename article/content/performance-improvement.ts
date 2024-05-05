d8a851c23809f0db1d7416aa16f6ba92d621b5fa
Brian Takita <brian.takita@gmail.com>
2024-05-05 01:29:19 -0400

    /articles/performance-improvement: + content,references

--- article/content/performance-improvement.ts
+++ article/content/performance-improvement.ts
@@ -1,33 +1,297 @@
 import { performance_improvement } from '@btakita/domain--server--peakexistence/feature'
 import { post_meta__validate } from '@rappstack/domain--server--blog/post'
-import { md__raw_ } from '@rappstack/ui--any/md'
+import { tb_a_ } from '@rappstack/ui--any/anchor'
+import { dl_tree_ } from '@rappstack/ui--any/dl'
 import { nl } from '@rappstack/ui--any/string'
+import { footnote__sup_, footnote__sup__sort } from '@rappstack/ui--server--blog/footnote'
 import { type request_ctx_T } from 'rebuildjs/server'
+import { sticky__dl_tree_props_ } from '../../sticky/index.js'
 export const meta_ = (ctx:request_ctx_T)=>post_meta__validate(ctx, {
 	pub_date: '2024-03-22T20:42:15.614Z',
 	...performance_improvement,
+	is_pillar: true,
+	description: `In the competitive world of sports, athletes continuously seek methods to enhance their performance, accelerate recovery, & extend their careers. Modern sports science integrates advanced therapies with traditional training. To provide athletes with the edge they need. This article explores several cutting-edge techniques, including altitude training, hyperbaric oxygen therapy, exercise with oxygen therapy, platelet-rich plasma, bone marrow aspirate, IV vitamin supplementation, sports nutrition, supplementation, hormone balancing, peptides, & natural pain relief therapies.`,
+	description_md: [
+		`In the competitive world of sports, athletes continuously seek methods to:`,
+		``,
+		`- enhance their performance`,
+		`- accelerate recovery`,
+		`- extend their careers`,
+		``,
+		`Modern sports science integrates advanced therapies with traditional training. To provide athletes with the edge they need. This article explores several cutting-edge techniques, including:`,
+		``,
+		`- altitude training`,
+		`- hyperbaric oxygen therapy`,
+		`- exercise with oxygen therapy`,
+		`- platelet-rich plasma`,
+		`- bone marrow aspirate`,
+		`- IV vitamin supplementation`,
+		`- sports nutrition`,
+		`- supplementation`,
+		`- hormone balancing`,
+		`- peptides`,
+		`- natural pain relief therapies`
+	].join(nl)
 })
 // @formatter:off
 // language=md
 export default (ctx:request_ctx_T)=>''
-+ md__raw_(ctx, [
-	`Conventional sports medicine treatments often provide short-term pain relief. Dr. Emenike’s cutting-edge therapies & performance plans go beyond the basics. Of ice, pain medications, & compression. Advanced approaches focus on preventing future injuries. While enhancing an athlete’s current performance. Featured treatments include:`,
-	`- hyperbaric oxygen therapy`,
-	`- platelet-rich plasma`,
-	`- bone marrow aspirate`,
-	`- IV vitamin supplementation`,
-	``,
-	`Dr. Emenike draws on his endurance athletic experience. To understand what it takes to support & optimize one’s health to excel in sports. Adapting to injuries & overtraining, he emphasizes with athletes & their struggles. Starting with the protocols that helped him recover & reach peak performance. He has honed expertise in:`,
-	`- sports nutrition`,
-	`- supplementation`,
-	`- hormone balancing`,
-	`- natural pain relief therapies`,
-	``,
-	`With Dr. Emenike’s guidance, athletes can boost their performance. And maintain long-term health. Individual & comprehensive care ensures each athlete is well-prepared. To achieve goals & stay in top shape for competitions.`,
-	``,
-	`Dr. Emenike emphasizes the importance of nutrition & hydration, especially during altitude training. Lower oxygen levels trigger heightened stress hormone responses. Requiring the need for increased caloric intake, particularly carbohydrates. To replenish glycogen stores & meet the heightened energy demands at high altitudes.`,
-	``,
-	`Proper hydration is critical for maintaining peak performance & well-being. Athletes should monitor hydration levels. By weighing before & after training. Optimizing fluid intake to suit individual needs.`,
-	``,
-	`Dr. Emenike’s expert guidance brings confidence to athletes to pursue their endeavors. Focusing on injury prevention, performance optimization, long-term health, & well-being.`
-].join(nl))
++ dl_tree_({ ctx, _: sticky__dl_tree_props_ }, ()=>[
+	[`## Performance Improvement`, [
+		dl_tree_({ ctx, _: sticky__dl_tree_props_ }, ()=>[
+			[`### Altitude Training`, [
+				`Altitude training is a well-established method that improves athletic performance. By training at high altitudes where oxygen levels are lower. This type of training increases the body's red blood cell count. Enhancing oxygen delivery & aerobic capacity when the athlete returns to lower altitudes. Altitude training is a staple in many elite athletes' regimens with benefits including${footnote__sup__sort([ref_p2_1_footnote__sup_(ctx),ref_p3_1_footnote__sup_(ctx)])}:`,
+				``,
+        `- improved endurance`,
+        `- increased lactic acid tolerance`,
+        `- enhanced oxygen flow to muscles`].join(nl)],
+			[`### Hyperbaric Oxygen Therapy (HBOT)`, [
+				`HBOT involves breathing pure oxygen in a pressurized chamber. Which significantly increases oxygen concentration in the blood & tissues. This therapy aids in faster recovery from sports injuries. By reducing inflammation & promoting healing at the cellular level${ref_p1_1_footnote__sup_(ctx)}. Athletes report reduced recovery times & enhanced healing of acute injuries. Making HBOT a valuable addition to sports medicine${footnote__sup__sort([ref_p3_2_1_footnote__sup_(ctx),ref_p3_2_2_footnote__sup_(ctx)])}.`].join(nl)],
+			[`### Exercise with Oxygen Therapy (EWOT)`, [
+				`EWOT enhances traditional exercise. By having athletes breathe higher concentrations of oxygen during physical activity. This method increases the oxygen delivered to muscles, improving${footnote__sup__sort([ref_p1_7_footnote__sup_(ctx),ref_p3_3_footnote__sup_(ctx)])}:`,
+				``,
+        `- energy`,
+        `- endurance`,
+        `- post-exercise recovery`,
+				``,
+        `EWOT maximizes training results & accelerates recovery. Providing benefits similar to altitude training but in a more controlled environment.`].join(nl)],
+			[`### Platelet-Rich Plasma (PRP)`, [
+				`PRP therapy involves injecting concentrated platelets from the athlete’s blood. Into injured areas to accelerate healing. This therapy is particularly effective in${ref_p3_4_footnote__sup_(ctx)}:`,
+				``,
+        `- treating soft tissue injuries`,
+        `- enhancing the healing process`].join(nl)],
+			[`### Bone Marrow Aspirate`, [
+				`Bone marrow aspirate therapy involves extracting stem cells from bone marrow. Using them to treat & manage sports injuries. These stem cells have the potential to differentiate into various types of tissues. Which is crucial for repairing damaged tissues${ref_p3_5_footnote__sup_(ctx)}.`].join(nl)],
+			[`### IV Vitamin Supplementation`, [
+				`IV vitamin therapy delivers high doses of vitamins & minerals directly into the bloodstream. Bypassing the digestive system for maximum absorption. This therapy is used to${ref_p3_6_footnote__sup_(ctx)}:`,
+				``,
+        `- boost overall wellness`,
+        `- enhance immune function`,
+        `- improve recovery times after intense physical exertion`].join(nl)],
+			[`### Sports Nutrition`, [
+				`Proper nutrition is crucial for peak athletic performance. Sports nutrition focuses on optimizing intake of nutrients to improve performance & recovery. Such as${ref_p3_7_footnote__sup_(ctx)}:`,
+				``,
+        `- proteins`,
+        `- carbohydrates`,
+        `- fats`].join(nl)],
+			[`### Supplementation`, [
+				`Supplements such as creatine, beta-alanine, & branched-chain amino acids (BCAAs). Are often used to enhance energy production & muscle recovery. With supplements selected to support specific aspects of athletic performance & recovery${ref_p3_8_footnote__sup_(ctx)}.`].join(nl)],
+			[`### Hormone Balancing`, [
+				`Balancing hormones is critical for optimizing physical performance & recovery. Hormones such as testosterone & human growth hormone significantly influence${ref_p3_9_footnote__sup_(ctx)}:`,
+				``,
+        `- muscle growth`,
+        `- energy levels`,
+        `- recovery rates`].join(nl)],
+			[`### Peptides`, [
+				`Peptides are short chains of amino acids that can influence various biological processes. Certain peptides promote growth hormone release, which is beneficial for${ref_p3_10_footnote__sup_(ctx)}:`,
+				``,
+        `- muscle growth`,
+        `- fat loss`,
+        `- recovery`].join(nl)],
+			[`### Natural Pain Relief Therapies`, [
+				`Natural pain relief methods, such as acupuncture, massage, & chiropractic care. Are integral to an athlete's recovery & performance strategy. These therapies help manage pain, improve range of motion, & reduce recovery time${ref_p3_11_footnote__sup_(ctx)}.`].join(nl)],
+		])].join(nl)],
+	[`## Peak Existence`, [
+		`Conventional sports medicine treatments often provide short-term pain relief. Dr. Emenike’s cutting-edge therapies & performance plans go beyond the basics. Of ice, pain medications, & compression. Advanced approaches focus on preventing future injuries. While enhancing an athlete’s current performance. Featured treatments include:`,
+		`- hyperbaric oxygen therapy`,
+		`- platelet-rich plasma`,
+		`- bone marrow aspirate`,
+		`- IV vitamin supplementation`,
+		``,
+		`Dr. Emenike draws on his endurance athletic experience. To understand what it takes to support & optimize one’s health to excel in sports. Adapting to injuries & overtraining, he emphasizes with athletes & their struggles. Starting with the protocols that helped him recover & reach peak performance. He has honed expertise in:`,
+		`- sports nutrition`,
+		`- supplementation`,
+		`- hormone balancing`,
+		`- natural pain relief therapies`,
+		``,
+		`With Dr. Emenike’s guidance, athletes can boost their performance. And maintain long-term health. Individual & comprehensive care ensures each athlete is well-prepared. To achieve goals & stay in top shape for competitions.`,
+		``,
+		`Dr. Emenike emphasizes the importance of nutrition & hydration, especially during altitude training. Lower oxygen levels trigger heightened stress hormone responses. Requiring the need for increased caloric intake, particularly carbohydrates. To replenish glycogen stores & meet the heightened energy demands at high altitudes.`,
+		``,
+		`Proper hydration is critical for maintaining peak performance & well-being. Athletes should monitor hydration levels. By weighing before & after training. Optimizing fluid intake to suit individual needs.`,
+		``,
+		`Dr. Emenike’s expert guidance brings confidence to athletes to pursue their endeavors. Focusing on injury prevention, performance optimization, long-term health, & well-being.`
+	].join(nl)],
+	])
+// @formatter:on
+function ref_p1_1_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'p1_1'
+	}, [
+		tb_a_({
+			href: 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3382683/',
+			nofollow: true,
+			wrap: true,
+		}, 'Hyperbaric Oxygen Effects on Sports Injuries')
+	])
+}
+/*Good Article Idea*/
+function ref_p1_7_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'p1_7'
+	}, [
+		tb_a_({
+			href: 'https://optimalbreathing.com/pages/ewot',
+			nofollow: true,
+			wrap: true,
+		}, 'Exercise with Oxygen Therapy (EWOT) - An In-Depth Guide')
+	])
+}
+function ref_p2_1_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'p2_1'
+	}, [
+		tb_a_({
+			href: 'https://www.healthline.com/health/altitude-training',
+			nofollow: true,
+			wrap: true,
+		}, 'All About High Altitude Fitness Training')
+	])
+}
+function ref_p3_1_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'p3_1'
+	}, [
+		tb_a_({
+			href: 'https://journals.physiology.org/doi/full/10.1152/jappl.1997.83.1.102',
+			nofollow: true,
+			wrap: true,
+		}, '“Living high-training low”: effect of moderate-altitude acclimatization with low-altitude training on performance')
+	])
+}
+function ref_p3_2_1_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'p3_2_1'
+	}, [
+		tb_a_({
+			href: 'https://www.semanticscholar.org/paper/The-Role-of-Hyperbaric-Oxygen-Therapy-in-Sports-Babul-Rhodes/2db23f4211d02bb6feed79dcabf803ca22aff2a8',
+			nofollow: true,
+			wrap: true,
+		}, 'The Role of Hyperbaric Oxygen Therapy in Sports Medicine')
+	])
+}
+function ref_p3_2_2_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'p3_2_2'
+	}, [
+		tb_a_({
+			href: 'https://www.researchgate.net/publication/230624734_Hyperbaric_Oxygen_Effects_on_Sports_Injuries',
+			nofollow: true,
+			wrap: true,
+		}, 'Hyperbaric Oxygen Effects on Sports Injuries')
+	])
+}
+function ref_p3_3_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'p3_3'
+	}, [
+		tb_a_({
+			href: 'https://www.sportingedgeuk.com/library/medical/IHT_Research_Soviet_Union_Tatiana_Serebrovskaya.pdf',
+			nofollow: true,
+			wrap: true,
+		}, 'Intermittent Hypoxia Research in the Former Soviet Union and the Commonwealth of Independent States: History and Review of the Concept and Selected Applications')
+	])
+}
+function ref_p3_4_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'p3_4'
+	}, [
+		tb_a_({
+			href: 'https://www.yesprpkit.com/pdf/studies/PRPClinicalApplications.pdf',
+			nofollow: true,
+			wrap: true,
+		}, 'Plat~let~Rich Plasma From Basic Science to Clinical Applications')
+	])
+}
+function ref_p3_5_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'p3_5'
+	}, [
+		tb_a_({
+			href: 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3824897/',
+			nofollow: true,
+			wrap: true,
+		}, 'Benefits of small volume and small syringe for bone marrow aspirations of mesenchymal stem cells')
+	])
+}
+function ref_p3_6_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'p3_6'
+	}, [
+		tb_a_({
+			href: 'https://altmedrev.com/wp-content/uploads/2019/02/v7-5-389.pdf',
+			nofollow: true,
+			wrap: true,
+		}, 'Intravenous Nutrient Therapy: the “Myers’ Cocktail”')
+	])
+}
+function ref_p3_7_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'p3_7'
+	}, [
+		tb_a_({
+			href: 'https://www.researchgate.net/publication/51208260_Carbohydrates_for_training_and_competition',
+			nofollow: true,
+			wrap: true,
+		}, 'Carbohydrates for training and competition')
+	])
+}
+function ref_p3_8_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'p3_8'
+	}, [
+		tb_a_({
+			href: 'https://crearene.com/wp-content/uploads/2021/04/ZD-International-Society-of-Sports-Nutritionposition-safety-and-efficacy-ofcreatine-supplementation-in-exercise-sport-and-medicine.pdf',
+			nofollow: true,
+			wrap: true,
+		}, 'International Society of Sports Nutrition position stand: safety and efficacy of creatine supplementation in exercise, sport, and medicine')
+	])
+}
+function ref_p3_9_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'p3_9'
+	}, [
+		tb_a_({
+			href: 'https://www.mja.com.au/system/files/issues/185_08_161006/han10124_fm.pdf',
+			nofollow: true,
+			wrap: true,
+		}, 'Testosterone: use, misuse and abuse')
+	])
+}
+function ref_p3_10_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'p3_10'
+	}, [
+		tb_a_({
+			href: 'https://www.researchgate.net/publication/260522903_Endocrine_Profiles_in_693_Elite_Athletes_in_the_Post-Competition_Setting',
+			nofollow: true,
+			wrap: true,
+		}, 'Endocrine Profiles in 693 Elite Athletes in the Post-Competition Setting.')
+	])
+}
+function ref_p3_11_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 'p3_11'
+	}, [
+		tb_a_({
+			href: 'https://www.mvclinic.es/wp-content/uploads/2009_Tough_SR_Meta-analysis_Acupuncture-and-dry-needling1.pdf',
+			nofollow: true,
+			wrap: true,
+		}, 'Acupuncture and dry needling in the management of myofascial trigger point pain: A systematic review and meta-analysis of randomised controlled trials')
+	])
+}
