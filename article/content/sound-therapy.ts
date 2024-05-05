7e77141a31ef0ad48e03ef95aa893f32da05d059
Brian Takita <brian.takita@gmail.com>
2024-05-05 03:00:25 -0400

    /articles/sound-therapy: + references

--- article/content/sound-therapy.ts
+++ article/content/sound-therapy.ts
@@ -18,7 +18,7 @@ export const meta_ = (ctx:request_ctx_T)=>post_meta__validate(ctx, {
 export default (ctx:request_ctx_T)=>''
 + dl_tree_({ ctx, _: sticky__dl_tree_props_ }, ()=>[
 	[`## The Foundations of Sound Therapy`, [
-		`Sound therapy is also known as sound healing or vibrational medicine. Sound therapy's predicated on the principle that matter vibrates at specific frequencies. Including the human body, vibrates at specific frequencies. Health & well-being influenced by these vibrational states. Disrupting or unbalancing the body's natural frequencies leads to physical or emotional disharmony. Sound therapy seeks to restore balance & promote healing. By using various musical instruments & vocal techniques to produce therapeutic sounds & vibrations${ref_s1_2_footnote__sup_(ctx)}.`].join(nl)],
+		`Sound therapy is also known as sound healing or vibrational medicine. Sound therapy's predicated on the principle that matter vibrates at specific frequencies. Including the human body, vibrates at specific frequencies. Health & well-being influenced by these vibrational states. Disrupting or unbalancing the body's natural frequencies leads to physical or emotional disharmony. Sound therapy seeks to restore balance & promote healing. By using various musical instruments & vocal techniques to produce therapeutic sounds & vibrations${footnote__sup__sort([ref_s1_2_footnote__sup_(ctx),ref_s2_5_footnote__sup_(ctx),ref_s2_9_footnote__sup_(ctx)])}.`].join(nl)],
 	[`## How Does ${mobile_hidden__span_([`Sound Therapy`, `It`])} Work?`, [
 		`Several theories explain the potential mechanisms of sound therapy:`,
 		dl_tree_({ ctx, _: sticky__dl_tree_props_ }, ()=>[
@@ -33,7 +33,8 @@ export default (ctx:request_ctx_T)=>''
 				`- facilitate a sense of calm`].join(nl)],
 		])].join(nl)],
 	[`## The Science of Sound`, [
-		`The therapeutic effects of sound ground in both physics & neurobiology. Sound waves transmit through the air as vibrations. Interacting with the body in a profound way. These vibrations can stimulate the release of nitric oxide. A molecule that promotes healing & reduces inflammation. Sound therapy influences brainwave patterns. Leading to:`,
+		`The therapeutic effects of sound ground in both physics & neurobiology. Sound waves transmit through the air as vibrations. Interacting with the body in a profound way. These vibrations can stimulate the release of nitric oxide. A molecule that promotes healing & reduces inflammation. Sound therapy influences brainwave patterns. Leading to${ref_s2_5_footnote__sup_(ctx)}:`,
+		``,
 		`- increased relaxation`,
 		`- reduced stress levels`,
 		`- enhanced cognitive function`].join(nl)],
@@ -41,9 +42,9 @@ export default (ctx:request_ctx_T)=>''
 		`Sound therapy encompasses a wide range of techniques & instruments. Offering unique vibrational qualities & benefits. Some of the most widely used modalities include:`,
 		dl_tree_({ ctx, _: sticky__dl_tree_props_ }, ()=>[
 			[`### Sound Baths`, [
-				`Immersive experiences using crystal singing bowls, gongs, & other resonant instruments. To bathe participants in a wash of vibrations. Participants lie down & are "bathed" in sound waves. Produced by gongs, singing bowls, or other instruments. These sessions promote deep relaxation & stress relief${footnote__sup__sort([ref_s1_2_footnote__sup_(ctx),ref_s1_4_footnote__sup_(ctx)])}.`].join(nl)],
+				`Immersive experiences using crystal singing bowls, gongs, & other resonant instruments. To bathe participants in a wash of vibrations. Participants lie down & are "bathed" in sound waves. Produced by gongs, singing bowls, or other instruments. These sessions promote deep relaxation & stress relief${footnote__sup__sort([ref_s2_1_footnote__sup_(ctx),ref_s1_2_footnote__sup_(ctx),ref_s1_4_footnote__sup_(ctx)])}.`].join(nl)],
 			[`### Binaural Beats`, [
-				`A method involving the delivery of two slightly different frequencies to each ear. Encouraging the brain to perceive a third tone that promotes relaxation & focus. The brain perceives these tones as a single beat. Altering brainwave activity to${ref_s1_1_footnote__sup_(ctx)}:`,
+				`A method involving the delivery of two slightly different frequencies to each ear. Encouraging the brain to perceive a third tone that promotes relaxation & focus. The brain perceives these tones as a single beat. Altering brainwave activity to${footnote__sup__sort([ref_s2_7_footnote__sup_(ctx),ref_s1_1_footnote__sup_(ctx)])}:`,
 				``,
         `- reduce stress`,
         `- enhance relaxation`,
@@ -51,15 +52,15 @@ export default (ctx:request_ctx_T)=>''
 			[`### Tuning Forks`, [
 				`Used to apply specific vibrations directly to the body. Helping to release tension & energy blockages. This method uses calibrated metal tuning forks. To apply specific vibrations to different parts of the body. It is similar to acupuncture. But uses sound frequencies instead of needles to stimulate the body's energy pathways${ref_s1_3_footnote__sup_(ctx)}.`].join(nl)],
 			[`### Vibroacoustic Therapy`, [
-				`Low-frequency vibrations are transmitted to the body. Through speakers embedded in furniture or mats. This method is particularly effective for reducing pain & improving mobility. In conditions like Parkinson's disease & fibromyalgia${ref_s1_5_footnote__sup_(ctx)}.`].join(nl)],
+				`Low-frequency vibrations are transmitted to the body. Through speakers embedded in furniture or mats. This method is particularly effective for reducing pain & improving mobility. In conditions like Parkinson's disease & fibromyalgia${footnote__sup__sort([ref_s2_4_footnote__sup_(ctx),ref_s2_10_footnote__sup_(ctx),ref_s1_5_footnote__sup_(ctx)])}.`].join(nl)],
 			[`### Music Therapy`, [
-				`Led by trained therapists, using music to address emotional, cognitive, or physical needs.`].join(nl)],
+				`Led by trained therapists, using music to address emotional, cognitive, or physical needs${footnote__sup__sort([ref_s2_5_footnote__sup_(ctx),ref_s2_9_footnote__sup_(ctx)])}.`].join(nl)],
 			[`### Tibetan Singing Bowls`, [
-				`These ancient bowls produce rich overtones that promote deep meditation & relaxation.`].join(nl)],
+				`These ancient bowls produce rich overtones that promote deep meditation & relaxation${ref_s2_1_footnote__sup_(ctx)}.`].join(nl)],
 			[`### Gongs`, [
-				`Produce a broad spectrum of sound that stimulates a deep meditative state & relaxation.`].join(nl)],
+				`Produce a broad spectrum of sound that stimulates a deep meditative state & relaxation${ref_s2_1_footnote__sup_(ctx)}.`].join(nl)],
 			[`### Guided Meditations with Sound`, [
-				`Voice guidance combined with ambient music or nature sounds. Facilitate relaxation & introspection.`].join(nl)],
+				`Voice guidance combined with ambient music or nature sounds. Facilitate relaxation & introspection${ref_s2_1_footnote__sup_(ctx)}.`].join(nl)],
 			[`### Voice`, [
 				`Chanting, toning, & overtone singing help you find your natural resonance. Facilitating emotional release & connection.`].join(nl)],
 		])].join(nl)],
@@ -80,22 +81,17 @@ export default (ctx:request_ctx_T)=>''
 				`- clarity`,
 				`- cognitive function`].join(nl)],
 			[`### Manage Pain`, [
-				`Vibrational medicine has shown promise in alleviating chronic pain & discomfort. Offering a non-invasive alternative to traditional pain management strategies. Certain sound frequencies reduce pain by altering the body's pain perception pathways. Including${ref_s1_3_footnote__sup_(ctx)}:`,
+				`Vibrational medicine has shown promise in alleviating chronic pain & discomfort. Offering a non-invasive alternative to traditional pain management strategies. Certain sound frequencies reduce pain by altering the body's pain perception pathways. Including${footnote__sup__sort([ref_s1_3_footnote__sup_(ctx),ref_s2_7_footnote__sup_(ctx),ref_s2_10_footnote__sup_(ctx)])}:`,
 				``,
         `- chronic pain`,
         `- postoperative pain`].join(nl)],
 			[`### Improve Mood`, [
-				`Uplifting sounds may boost mood & offer temporary relief for depression.`].join(nl)],
+				`Uplifting sounds may boost mood & offer temporary relief for depression${ref_s2_5_footnote__sup_(ctx)}.`].join(nl)],
 			[`### Enhanced Meditation`, [
 				`Sound can serve as a focus or enhance a meditative state.`].join(nl)],
-			[`### Cognitive Function`, [
-				`Therapeutic sound enhances learning & creativity by improving:`,
-				`- focus`,
-				`- clarity`,
-				`- cognitive function`].join(nl)],
 		])].join(nl)],
 	[`## Clinical Applications & Research`, [
-		`Sound therapy is increasingly being incorporated into clinical settings. To complement traditional medical treatments. Research has demonstrated its effectiveness in various settings, including hospitals. Where it helps patients manage pain & anxiety. It is also used in mental health clinics. To support treatments for conditions like PTSD & depression${footnote__sup__sort([ref_s1_2_footnote__sup_(ctx),ref_s1_8_footnote__sup_(ctx)])}.`].join(nl)],
+		`Sound therapy is increasingly being incorporated into clinical settings. To complement traditional medical treatments. Research has demonstrated its effectiveness in various settings, including hospitals. Where it helps patients manage pain & anxiety. It is also used in mental health clinics. To support treatments for conditions like PTSD & depression${footnote__sup__sort([ref_s1_2_footnote__sup_(ctx),ref_s1_8_footnote__sup_(ctx),ref_s2_7_footnote__sup_(ctx),ref_s2_10_footnote__sup_(ctx)])}.`].join(nl)],
 	[`## Who Might Consider ${mobile_hidden__span_([`Sound Therapy`, `It`])}?`, [
 		`Sound therapy is generally safe & can be explored by anyone interested in:`,
 		`- Stress Reduction`,
@@ -207,5 +203,77 @@ function ref_s1_8_footnote__sup_(ctx:request_ctx_T) {
 			nofollow: true,
 			wrap: true,
 		}, 'Sound Healing Therapy: What is it and How Can It Benefit You?')
+	])
+}
+function ref_s2_1_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 's2_1'
+	}, [
+		tb_a_({
+			href: 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5871151/',
+			nofollow: true,
+			wrap: true,
+		}, 'Effects of Singing Bowl Sound Meditation on Mood, Tension, and Well-being: An Observational Study')
+	])
+}
+function ref_s2_4_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 's2_4'
+	}, [
+		tb_a_({
+			href: 'https://www.researchgate.net/publication/258172758_Contemporary_Vibroacoustic_Therapy_Perspectives_on_Clinical_Practice_Research_and_Training',
+			nofollow: true,
+			wrap: true,
+		}, 'Contemporary Vibroacoustic Therapy: Perspectives on Clinical Practice, Research, and Training')
+	])
+}
+function ref_s2_5_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 's2_5'
+	}, [
+		tb_a_({
+			href: 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3734071/',
+			nofollow: true,
+			wrap: true,
+		}, 'The Effect of Music on the Human Stress Response')
+	])
+}
+function ref_s2_7_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 's2_7'
+	}, [
+		tb_a_({
+			href: 'https://sci-hub.se/10.1007/s00426-018-1066-8',
+			nofollow: true,
+			wrap: true,
+		}, 'Efficacy of binaural auditory beats in cognition, anxiety, and pain perception: a meta‑analysis')
+	])
+}
+function ref_s2_9_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 's2_9'
+	}, [
+		tb_a_({
+			href: 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5893695/',
+			nofollow: true,
+			wrap: true,
+		}, 'The hidden therapist: evidence for a central role of music in psychedelic therapy')
+	])
+}
+function ref_s2_10_footnote__sup_(ctx:request_ctx_T) {
+	return footnote__sup_({
+		ctx,
+		id: 's2_10'
+	}, [
+		tb_a_({
+			href: 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8984038/',
+			nofollow: true,
+			wrap: true,
+		}, 'Exploring vibroacoustic therapy in adults experiencing pain: a scoping review')
 	])
 }
