68fd542970acff69e09d624a2441a920b828be73
Brian Takita <brian.takita@gmail.com>
2024-05-26 19:06:42 -0400

    ./app: /: server: - ∋ sticky__breadcrumbs__nav_class,sticky__breadcrumbs__nav_class
    ./ctx: peakexistence_request_ctx__ensure: + ∋ sticky__breadcrumbs__nav_class,sticky__breadcrumbs__nav_class

--- app/articles/index.server.ts
+++ app/articles/index.server.ts
@@ -3,12 +3,10 @@ import './index.css'
 import { post__doc_html_, posts__doc_html_ } from '@btakita/ui--server--peakexistence/post'
 import { page_num_ } from '@rappstack/domain--server--blog/page'
 import { blog_post_slug_or_page_num__set } from '@rappstack/domain--server--blog/post'
-import { breadcrumbs__nav_class__set } from '@rappstack/ui--server--blog/breadcrumb'
 import { blog_post__estimate_read_minutes__wait } from '@rappstack/ui--server--blog/post'
 import { Elysia } from 'elysia'
 import { html_response__new, middleware_ } from 'relysjs/server'
 import { blog_site } from '../../config.js'
-import { sticky__breadcrumbs__nav_class } from '../../sticky/index.js'
 import { peakexistence_request_ctx__ensure } from '../../ctx/index.js'
 export default middleware_(middleware_ctx=>
 	new Elysia({
@@ -28,7 +26,6 @@ export default middleware_(middleware_ctx=>
 				context,
 				{ blog_site })
 			blog_post_slug_or_page_num__set(ctx, slug_or_page_num)
-			breadcrumbs__nav_class__set(ctx, sticky__breadcrumbs__nav_class)
 			// /** @see {import('@rappstack/ui--browser/sticky').sticky_stuck__hyop} */
 			// breadcrumbs__nav_hyop__set(ctx, 'sticky_stuck__hyop')
 			return html_response__new(
--- ctx/index.ts
+++ ctx/index.ts
@@ -1,8 +1,10 @@
 import { post_path_prefix__set } from '@rappstack/domain--any--blog/post'
 import { blog_request_ctx__ensure, type blog_request_ctx__ensure_config_T } from '@rappstack/domain--server--blog/ctx'
 import { marked__set } from '@rappstack/ui--any/md'
+import { breadcrumbs__nav_class__set } from '@rappstack/ui--server--blog/breadcrumb'
 import { app_marked_ } from '@rappstack/ui--server/md'
 import { type middleware_ctx_T } from 'rebuildjs/server'
+import { sticky__breadcrumbs__nav_class } from '../sticky/index.js'
 export function peakexistence_request_ctx__ensure(
 	middleware_ctx:middleware_ctx_T,
 	context:{
@@ -14,5 +16,6 @@ export function peakexistence_request_ctx__ensure(
   const ctx = blog_request_ctx__ensure(middleware_ctx, context, config)
 	post_path_prefix__set(ctx, 'articles')
 	marked__set(ctx, app_marked_(ctx))
+	breadcrumbs__nav_class__set(ctx, sticky__breadcrumbs__nav_class)
 	return ctx
 }
